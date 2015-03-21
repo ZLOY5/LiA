1ba54f3d1c647b7cd3d061f72c9c65d5b29d5dbf
ZLOY5 <ZLOY5@ukr.net>
2015-03-21 11:39:46 +0200

    2

---
+++ game/dota_addons/life_in_arena/scripts/vscripts/LiA_Common.lua
@@ -0,0 +1,97 @@
+
+
+function CalcPlayers()
+	local playercounter = 0
+	for nPlayerID = 0, DOTA_MAX_PLAYERS-1 do
+		if PlayerResource:IsValidPlayer(nPlayerID)  then 
+			playercounter=playercounter+1
+		end
+	end	
+	return playercounter
+end
+
+function RespawnAllHeroes() --говно крашит игру
+	DoWithAllHeroes(function(hero)
+		if not hero:IsAlive() then
+			hero:RespawnHero(false,false,false)
+			FindClearSpaceForUnit(hero, hero:GetAbsOrigin(), false)
+		end
+	end)
+end
+
+function ResetAllAbilitiesCooldown(unit)
+	local abilities = unit:GetAbilityCount()
+	for i = 1, abilities do
+		unit:GetAbilityByIndex(i):EndCooldown()
+	end
+end
+
+function DoWithAllHeroes(whatDo)
+	if type(whatDo) ~= "function" then
+		print("DoWithAllHeroes:not func")
+		return
+	end
+	local heroes = HeroList:GetAllHeroes() 
+	for i = 1, #heroes do
+		whatDo(heroes[i])
+	end
+end
+
+function ShowCenterMessage(msg, dur)
+	FireGameEvent("show_center_message",{message = msg,duration = dur}) 
+end
+
+function GetItemInInventory(unit,itemName)
+	for i = 0, 5 do
+		local Item = unit:GetItemInSlot(i) 
+		if Item:GetName() == itemName then
+			return Item 
+		end
+	end
+	return nil
+end
+
+POPUP_SYMBOL_PRE_PLUS = 0
+POPUP_SYMBOL_PRE_MINUS = 1
+POPUP_SYMBOL_PRE_SADFACE = 2
+POPUP_SYMBOL_PRE_BROKENARROW = 3
+POPUP_SYMBOL_PRE_SHADES = 4
+POPUP_SYMBOL_PRE_MISS = 5
+POPUP_SYMBOL_PRE_EVADE = 6
+POPUP_SYMBOL_PRE_DENY = 7
+POPUP_SYMBOL_PRE_ARROW = 8
+
+POPUP_SYMBOL_POST_EXCLAMATION = 0
+POPUP_SYMBOL_POST_POINTZERO = 1
+POPUP_SYMBOL_POST_MEDAL = 2
+POPUP_SYMBOL_POST_DROP = 3
+POPUP_SYMBOL_POST_LIGHTNING = 4
+POPUP_SYMBOL_POST_SKULL = 5
+POPUP_SYMBOL_POST_EYE = 6
+POPUP_SYMBOL_POST_SHIELD = 7
+POPUP_SYMBOL_POST_POINTFIVE = 8
+
+function PopupNumbers(target, pfx, color, lifetime, number, presymbol, postsymbol)
+    local pfxPath = string.format("particles/msg_fx/msg_%s.vpcf", pfx)
+    local pidx = ParticleManager:CreateParticle(pfxPath, PATTACH_ABSORIGIN_FOLLOW, target) -- target:GetOwner()
+
+    local digits = 0
+    if number ~= nil then
+        digits = #tostring(number)
+    end
+    if presymbol ~= nil then
+        digits = digits + 1
+    end
+    if postsymbol ~= nil then
+        digits = digits + 1
+    end
+
+    ParticleManager:SetParticleControl(pidx, 1, Vector(tonumber(presymbol), tonumber(number), tonumber(postsymbol)))
+    ParticleManager:SetParticleControl(pidx, 2, Vector(lifetime, digits, 0))
+    ParticleManager:SetParticleControl(pidx, 3, color)
+end
+
+--[[FireGameEvent( "console_command", { pID = player_ID, command = "Kappa" }) 
+for any command 
+<3 
+ConsoleCommands.swf or something ]]
