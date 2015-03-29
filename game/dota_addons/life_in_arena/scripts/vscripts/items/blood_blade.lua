98fc4ccb6572ba895d6642a5e2e1f4ec0ad5102a
ZLOY5 <ZLOY5@ukr.net>
2015-03-29 19:59:47 +0300

    kjk

--- game/dota_addons/life_in_arena/scripts/vscripts/items/blood_blade.lua
+++ game/dota_addons/life_in_arena/scripts/vscripts/items/blood_blade.lua
@@ -1,5 +1,5 @@
 function modifier_item_blood_blade_datadriven_on_orb_impact(keys)
-	if keys.target:GetInvulnCount() == nil and not keys.target:IsMechanical() and keys.target:GetTeamNumber() ~= keys.caster:GetTeamNumber() then
+	if --[[keys.target:GetInvulnCount() == nil and]] not keys.target:IsMechanical() and keys.target:GetTeamNumber() ~= keys.caster:GetTeamNumber() then
 		keys.ability:ApplyDataDrivenModifier(keys.attacker, keys.attacker, "modifier_item_blood_blade_datadriven_lifesteal", {duration = 0.03})
 	end
 end
