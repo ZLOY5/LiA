077347cfda601a005a8acb4af9c2740ad823f833
MooMooKhan <korshunoffegor@yandex.ru>
2015-04-23 18:31:59 +0300

    Меч Единства

---
+++ game/dota_addons/life_in_arena/scripts/vscripts/items/SwordOfSolidarity.lua
@@ -0,0 +1,35 @@
+function Salvation(keys)	
+	keys.caster:EmitSound("DOTA_Item.Mekansm.Activate")
+	ParticleManager:CreateParticle("particles/items2_fx/mekanism.vpcf", PATTACH_ABSORIGIN_FOLLOW, keys.caster)
+
+	local nearby_allied_units = FindUnitsInRadius(keys.caster:GetTeam(), keys.caster:GetAbsOrigin(), nil, keys.HealRadius,
+		DOTA_UNIT_TARGET_TEAM_FRIENDLY, DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC, DOTA_UNIT_TARGET_FLAG_NONE, FIND_ANY_ORDER, false)
+		
+	for i, nearby_ally in ipairs(nearby_allied_units) do  --Restore health and play a particle effect for every found ally.
+			nearby_ally:Heal(keys.HealAmount, keys.caster)
+			nearby_ally:GiveMana(keys.HealAmount)
+			
+		nearby_ally:EmitSound("DOTA_Item.Mekansm.Target")
+		ParticleManager:CreateParticle("particles/items2_fx/mekanism_recipient.vpcf", PATTACH_ABSORIGIN_FOLLOW, nearby_ally)
+		
+		keys.ability:ApplyDataDrivenModifier(keys.caster, nearby_ally, "modifier_sword_or_solidarity_armor", nil)
+	end
+end
+
+function SalvationSecond(keys)	
+	keys.caster:EmitSound("DOTA_Item.Mekansm.Activate")
+	ParticleManager:CreateParticle("particles/items2_fx/mekanism.vpcf", PATTACH_ABSORIGIN_FOLLOW, keys.caster)
+
+	local nearby_allied_units = FindUnitsInRadius(keys.caster:GetTeam(), keys.caster:GetAbsOrigin(), nil, keys.HealRadius,
+		DOTA_UNIT_TARGET_TEAM_FRIENDLY, DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC, DOTA_UNIT_TARGET_FLAG_NONE, FIND_ANY_ORDER, false)
+		
+	for i, nearby_ally in ipairs(nearby_allied_units) do  --Restore health and play a particle effect for every found ally.
+			nearby_ally:Heal(keys.HealAmount, keys.caster)
+			nearby_ally:GiveMana(keys.HealAmount)
+			
+		nearby_ally:EmitSound("DOTA_Item.Mekansm.Target")
+		ParticleManager:CreateParticle("particles/items2_fx/mekanism_recipient.vpcf", PATTACH_ABSORIGIN_FOLLOW, nearby_ally)
+		
+		keys.ability:ApplyDataDrivenModifier(keys.caster, nearby_ally, "modifier_sword_or_solidarity_armor_2", nil)
+	end
+end
