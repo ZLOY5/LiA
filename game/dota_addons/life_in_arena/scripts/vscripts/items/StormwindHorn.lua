502c153211c427ee11494ee1e0f0be3b606de013
MooMooKhan <korshunoffegor@yandex.ru>
2015-05-02 01:05:24 +0300

    Переводы

--- game/dota_addons/life_in_arena/scripts/vscripts/items/StormwindHorn.lua
+++ game/dota_addons/life_in_arena/scripts/vscripts/items/StormwindHorn.lua
@@ -9,7 +9,9 @@ end
 
 function AuraNone(event)
  local unit = event.target
-	if not unit:HasModifier("orb_of_frost_aura_armor") and not unit:HasModifier("horn_aura_armor") and not unit:HasModifier("ice_sword_aura_armor") then
-		unit:SetBaseHealthRegen(unit.base_regen)
+	if not unit:HasModifier("orb_of_frost_aura_armor") and not unit:HasModifier("horn_aura_armor") and not unit:HasModifier("ice_sword_aura_armor") then unit:SetBaseHealthRegen(unit.base_regen)
+	elseif unit:HasModifier("ice_sword_aura_armor") then unit:SetBaseHealthRegen(unit.base_regen*2.20)
+	elseif not unit:HasModifier("ice_sword_aura_armor") and unit:HasModifier("orb_of_frost_aura_armor") then unit:SetBaseHealthRegen(unit.base_regen*1.80)
+	elseif not unit:HasModifier("ice_sword_aura_armor") and not unit:HasModifier("orb_of_frost_aura_armor") and unit:HasModifier("horn_aura_armor") then unit:SetBaseHealthRegen(unit.base_regen*1.50)
 		end
 end
