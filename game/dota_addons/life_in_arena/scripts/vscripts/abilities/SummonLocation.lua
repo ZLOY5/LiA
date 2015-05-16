b92bee23a18311f6201e12e06b4a56e172b29eef
ZLOY5 <ZLOY5@ukr.net>
2015-05-16 17:12:04 +0300

    теперь крипы от предметов появляются перед героем

---
+++ game/dota_addons/life_in_arena/scripts/vscripts/abilities/SummonLocation.lua
@@ -0,0 +1,12 @@
+function SummonLocation( event )
+    local caster = event.caster
+    local fv = caster:GetForwardVector()
+    local origin = caster:GetAbsOrigin()
+    
+    local front_position = origin + fv * 250
+
+    local result = { }
+    table.insert(result, front_position)
+
+    return result
+end
