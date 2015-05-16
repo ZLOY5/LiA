bb7041b978ebb3b7b89d9a49047eda6b58abebcd
ZLOY5 <ZLOY5@ukr.net>
2015-05-16 18:59:41 +0300

    исправил Чуму

--- game/dota_addons/life_in_arena/scripts/vscripts/abilities/plague.lua
+++ game/dota_addons/life_in_arena/scripts/vscripts/abilities/plague.lua
@@ -2,12 +2,22 @@ function OnIntervalThink(event)
 	local damage = event.damage
 	if damage >= event.target:GetHealth() then
 		damage = event.target:GetHealth()-1
-	end 
+	end
+	if not event.caster then
+		event.target:RemoveModifierByName("modifier_plague_aura_datadriven")
+	end
+	--[[print("OnThink")
+	for k,v in pairs(event) do 
+		print(k,v)
+	end
+	print("----------------------\n")]]
 	ApplyDamage({ victim = event.target, attacker = event.caster, damage = damage, damage_type = DAMAGE_TYPE_MAGICAL, ability = event.ability}) 
 end
 
 function modifier_plague_aura_datadriven_on_created(event)
+	--print("\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!CREATED!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n")
 	if event.target:HasModifier(event.modifier_name_damage) then
+		event.target:RemoveModifierByName(event.modifier_name_damage)
 		event.ability:ApplyDataDrivenModifier(event.caster, event.target, event.modifier_name_damage,{duration = -1}) 
 	end
 	Timers:CreateTimer(event.immunity_time,function()
@@ -18,6 +28,10 @@ function modifier_plague_aura_datadriven_on_created(event)
 end
 
 function modifier_plague_aura_datadriven_on_destroy(event)
+	--[[print("OnDestroy")
+	for k,v in pairs(event) do 
+		print(k,v)
+	end]]
 	if event.target:HasModifier(event.modifier_name_damage) and event.ability then
 		event.ability:ApplyDataDrivenModifier(event.caster, event.target, event.modifier_name_damage,{duration = event.plague_time}) 
 	end
