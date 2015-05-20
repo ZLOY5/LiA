edc21a3434f7cbf0336598928c7d1bbdc74f8fc1
MooMooKhan <korshunoffegor@yandex.ru>
2015-05-20 18:34:40 +0300

    Ульт Скелета

---
+++ game/dota_addons/life_in_arena/scripts/vscripts/heroes/SkeletonMage/DeathEnergy.lua
@@ -0,0 +1,8 @@
+function DealDamage( event )
+	local caster = event.caster
+	local ability = event.ability
+	local target = event.target
+	local targets = event.target_entities
+			ApplyDamage({ victim = target, attacker = caster, damage = (target:GetMaxHealth()*ability:GetLevelSpecialValueFor("damage_percentage", ability:GetLevel() - 1 )*0.01), damage_type = DAMAGE_TYPE_MAGICAL })	
+
+end
