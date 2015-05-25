2ac7cca303524dcddc3b0c968016c0a66079a312
cezarrr666 <666rim@mail.ru>
2015-05-25 20:44:36 +0500

    Охотница
    По пунктам:
    1. Не занимался вплотную эффектами, только механикой (просьба прилепите
    эффекты подходящие). Также нет звуков, иконок, описания.
    2. kv_heroes = LoadKeyValues("scripts/npc/npc_heroes.txt") -- 28 строчка
    файла MoonGlaive.lua
    не нужно удивляться что такого файла не существует, у автора спелла его
    тоже нет. без него всё работает, проверял. думаю лучше оставить как
    есть.
    3. Хотя конечно потестить можно (могу ошибаться конечно же с kv_heroes =
    LoadKeyValues("scripts/npc/npc_heroes.txt") ).

---
+++ game/dota_addons/life_in_arena/scripts/vscripts/heroes/Huntress/CurseOfElune.lua
@@ -0,0 +1,29 @@
+--[[
+
+]]
+
+
+
+function SetDamageFromHuntress( keys )
+	local target = keys.target
+	local damage_per_sec = keys.damage_per_sec
+	local damage_proc_for_creeps = keys.damage_proc_for_creeps
+	local damage_proc_for_heroes_and_mega = keys.damage_proc_for_heroes_and_mega
+	local calc_damage
+	local not_full_hp = target:GetMaxHealth() - target:GetHealth()
+	if target:IsHero() or string.find(target:GetUnitName(),"megaboss") then
+		calc_damage = not_full_hp * damage_proc_for_heroes_and_mega * 0.01 + damage_per_sec
+	else
+		calc_damage = not_full_hp * damage_proc_for_creeps * 0.01 + damage_per_sec
+	end
+	ApplyDamage(
+	{
+		victim = target,
+		attacker = keys.caster,
+		damage = calc_damage,
+		damage_type = DAMAGE_TYPE_MAGICAL
+	})
+end
+
+
+
