8c3bc1d5dd27b4ce93e16a0eb2dae529cd2ae032
ZLOY5 <ZLOY5@ukr.net>
2015-05-27 20:39:03 +0300

    переделал 3 скилл Морлока

--- game/dota_addons/life_in_arena/scripts/vscripts/heroes/Kret/lustForLife.lua
+++ game/dota_addons/life_in_arena/scripts/vscripts/heroes/Kret/lustForLife.lua
@@ -1,18 +1,16 @@
---[[
 
-]]
+function UpdateRegen(event)
+	local caster = event.caster
+	local ability = event.ability
 
-
-
-function SetHealKret( keys )
-	local caster = keys.caster
-	--
-	local procVal_porog = keys.procVal_porog
-	local procVal_hp = keys.procVal_hp
-	--
-	if ( caster:GetHealth()/caster:GetMaxHealth() )*100 < procVal_porog then
-		caster:Heal( (caster:GetMaxHealth()/100)*procVal_hp, caster )
+	while(caster:HasModifier("modifier_lust_for_life_health_regen")) do
+		caster:RemoveModifierByName("modifier_lust_for_life_health_regen")
 	end
 
+	local modifierCount = math.ceil((100 - caster:GetHealthPercent())/10)
+
+	for i = modifierCount, 0, -1 do
+		ability:ApplyDataDrivenModifier(caster, caster, "modifier_lust_for_life_health_regen", nil)
+	end
 end
 
