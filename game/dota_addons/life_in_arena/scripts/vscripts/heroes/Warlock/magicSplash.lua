5c03b3d13d11ffcecf4faef2380f9fe9bb48a20a
Nagibator230 <mamhigtt@gmail.com>
2015-05-31 21:02:38 +0300

    Update magicSplash.lua

--- game/dota_addons/life_in_arena/scripts/vscripts/heroes/Warlock/magicSplash.lua
+++ game/dota_addons/life_in_arena/scripts/vscripts/heroes/Warlock/magicSplash.lua
@@ -1,21 +1,24 @@
-
-
-function check_order( keys )
+function overload_check_order( keys )
 	local caster = keys.caster
 	local ability = keys.ability
-	ListenToGameEvent( 'dota_player_used_ability',
-		function(_, event)
-			local pl = EntIndexToHScript( event.PlayerID )
-			if pl then
-				local hero = pl:GetAssignedHero()
+	ListenToGameEvent( "dota_player_used_ability", function( event )
+			local ply = EntIndexToHScript( event.PlayerID )
+			-- Check if player existed
+			if ply then
+				local hero = ply:GetAssignedHero()
+				-- Check if it is corrent hero
 				if hero == caster then
-					local findAbility = hero:FindAbilityByName( event.abilityname )
-					if (caster == hero) and ( findAbility:GetClassname() == 'ability_datadriven') then
-						ability:ApplyDataDrivenModifier(caster, caster, "modifier_inflicts", {duration = 0.03})
+					-- Check if ability on cast bar is casted
+					local ability_count = caster:GetAbilityCount()
+					for i = 0, (ability_count - 1) do
+						local ability_at_slot = caster:GetAbilityByIndex( i )
+						if ability_at_slot and ability_at_slot:GetAbilityName() == event.abilityname then
+							ability:ApplyDataDrivenModifier( caster, caster, "modifier_overload_damage_datadriven", {} )
+							break
+						end
 					end
 				end
 			end
-		end
-	, {})
-	--ListenToGameEvent( 'dota_player_used_ability', ApplyCooldownReduction, {})
+		end, nil
+	)
 end
