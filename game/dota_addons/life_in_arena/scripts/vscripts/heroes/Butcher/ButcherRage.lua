2cfec81ba63a2dc24fd78d6d190dae3f31ef5429
MooMooKhan <korshunoffegor@yandex.ru>
2015-06-13 18:36:07 +0300

    Пират
    Я стал новым пиратом.
    Где же ты, мой фрегат?
    Все что мне в жизни надо
    Взять, а жизни я рад.

--- game/dota_addons/life_in_arena/scripts/vscripts/heroes/Butcher/ButcherRage.lua
+++ game/dota_addons/life_in_arena/scripts/vscripts/heroes/Butcher/ButcherRage.lua
@@ -5,8 +5,6 @@ function DealDamage( event )
 	local targets = event.target_entities
 	local damage_to_deal = (caster:GetHealth()*ability:GetLevelSpecialValueFor("hp_damage_percentage", ability:GetLevel() - 1 )*0.01)
 	for _,v in pairs(targets) do
-		if v ~= target then
 			ApplyDamage({ victim = v, attacker = caster, damage = damage_to_deal, damage_type = DAMAGE_TYPE_PHYSICAL })
-		end
 	end
 end
