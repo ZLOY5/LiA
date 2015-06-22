b96ef09a3794e89a86ac527d7515f9c1a70b0335
ZLOY5 <ZLOY5@ukr.net>
2015-06-22 17:53:54 +0300

    Исправлений вам принес

---
+++ game/dota_addons/life_in_arena/scripts/vscripts/heroes/Highlander/StormBolt.lua
@@ -0,0 +1,25 @@
+function StormBolt(event)
+	local caster = event.caster
+	local target = event.target
+	local ability = event.ability
+
+	local damage_main_target = ability:GetSpecialValueFor("damage_main_target_tooltip")
+	local damage_radius = ability:GetSpecialValueFor("damage_radius")
+
+	local stun_main_target_duration = ability:GetSpecialValueFor("stun_main_target_duration")
+	local stun_radius_duration = ability:GetSpecialValueFor("stun_radius_duration")
+
+	local radius = ability:GetSpecialValueFor("radius")
+
+	local targets = FindUnitsInRadius(caster:GetTeam(), target:GetAbsOrigin(), nil, radius, DOTA_UNIT_TARGET_TEAM_ENEMY, DOTA_UNIT_TARGET_ALL, DOTA_UNIT_TARGET_FLAG_NONE, FIND_ANY_ORDER, false)
+	
+	for _,unit in pairs(targets) do 
+		if unit == target then
+			ApplyDamage({victim = unit, attacker = caster, damage = damage_main_target, damage_type = DAMAGE_TYPE_MAGICAL})
+			ability:ApplyDataDrivenModifier(caster, unit, "modifier_stunned", {duration = stun_main_target_duration})
+		else
+			ApplyDamage({victim = unit, attacker = caster, damage = damage_radius, damage_type = DAMAGE_TYPE_MAGICAL})
+			ability:ApplyDataDrivenModifier(caster, unit, "modifier_stunned", {duration = stun_radius_duration})
+		end
+	end
+end
---
+++ game/dota_addons/life_in_arena/scripts/vscripts/heroes/VowenFromBlood/SoulBurn.lua
@@ -0,0 +1,18 @@
+function SoulBurn(event)
+	local caster = event.caster
+	local target = event.target
+	local ability = event.ability
+
+	local duration_hero = ability:GetSpecialValueFor("duration_hero")
+	local duration_creep = ability:GetSpecialValueFor("duration_creep")
+	local buff_duration = ability:GetSpecialValueFor("buff_duration")
+
+	ability:ApplyDataDrivenModifier(caster, caster, "voven_from_blood_soul_burn_buff", {duration = buff_duration})
+
+	if target:IsRealHero() then
+		ability:ApplyDataDrivenModifier(caster, target, "voven_from_blood_soul_burn_debuff", {duration = duration_hero})
+	else
+		ability:ApplyDataDrivenModifier(caster, target, "voven_from_blood_soul_burn_debuff", {duration = duration_creep})
+	end	
+
+end
