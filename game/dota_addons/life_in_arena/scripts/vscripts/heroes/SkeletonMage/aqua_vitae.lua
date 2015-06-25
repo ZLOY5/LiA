b6a97a90ec135ee93c035dbbd05605ec0a3d7ec1
cezarrr666 <666rim@mail.ru>
2015-06-25 23:46:08 +0500

    1 спелл скел мага фикс
    тест

---
+++ game/dota_addons/life_in_arena/scripts/vscripts/heroes/SkeletonMage/aqua_vitae.lua
@@ -0,0 +1,14 @@
+function DealDamageAndHeal( event )
+	local caster = event.caster
+	local ability = event.ability
+	local target = event.target
+	local damage = event.damage 
+	local heal= event.heal
+	--
+	if target:GetTeamNumber() ~= caster:GetTeamNumber() then
+		ApplyDamage({ victim = target, attacker = caster, damage = damage, damage_type = DAMAGE_TYPE_PHYSICAL, ability = ability })
+	else
+		target:Heal(heal, caster)	
+	end
+
+end
