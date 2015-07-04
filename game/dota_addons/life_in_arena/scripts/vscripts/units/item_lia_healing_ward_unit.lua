69abf9045cc9a6a64a1639841fc8211df753ecef
ZLOY5 <ZLOY5@ukr.net>
2015-07-04 13:59:21 +0300

    Вард теперь здание

---
+++ game/dota_addons/life_in_arena/scripts/vscripts/units/item_lia_healing_ward_unit.lua
@@ -0,0 +1,8 @@
+require('timers')
+
+function Spawn(entityKeyValues)
+	Timers:CreateTimer(0.01,function()
+		thisEntity:RemoveModifierByName("modifier_invulnerable")
+	end)
+	thisEntity:FindAbilityByName("healing_ward_ability"):SetLevel(1)
+end
