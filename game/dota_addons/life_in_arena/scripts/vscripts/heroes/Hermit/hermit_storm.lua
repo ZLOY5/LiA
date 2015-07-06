3bbedee6421fcc084e8f2aa8e6d32a7f27d52b6d
cezarrr666 <666rim@mail.ru>
2015-07-06 23:33:56 +0500

    Готовый Пустынник к тесту

---
+++ game/dota_addons/life_in_arena/scripts/vscripts/heroes/Hermit/hermit_storm.lua
@@ -0,0 +1,62 @@
+
+function createWave(keys)
+	--
+	local ability = keys.ability
+	local caster = keys.caster --hero
+	local target = keys.target
+	--
+	local wave_speed = ability:GetSpecialValueFor( "wave_speed" )
+	local wave_width = ability:GetSpecialValueFor( "wave_width" )
+	local vision_aoe = ability:GetSpecialValueFor( "vision_aoe" )
+	local radius = ability:GetSpecialValueFor( "radius" )
+	print("radius ", radius)
+	--
+	-- Get random point
+	local castPoint = caster:GetAbsOrigin()
+	local attackPoint
+	local castDistance = RandomInt( 0, radius )
+	local angle = RandomInt( 0, 360 )
+	local dy = castDistance * math.sin( angle )
+	local dx = castDistance * math.cos( angle )
+	local variant = RandomInt( 0, 4 )
+	--
+	if variant == 0 then			
+		attackPoint = Vector( castPoint.x - dx, castPoint.y + dy, castPoint.z )
+	elseif variant == 1 then		
+		attackPoint = Vector( castPoint.x + dx, castPoint.y + dy, castPoint.z )
+	elseif variant == 2 then		
+		attackPoint = Vector( castPoint.x + dx, castPoint.y - dy, castPoint.z )
+	else										
+		attackPoint = Vector( castPoint.x - dx, castPoint.y - dy, castPoint.z )
+	end
+	print(attackPoint)
+	--local ti = radius/wave_speed
+	--
+    local info = {
+		Ability = ability,
+		EffectName = "particles/units/heroes/hero_morphling/morphling_waveform.vpcf",
+		
+		--"particles/units/heroes/hero_ancient_apparition/ancient_apparition_ambient.vpcf",
+		
+		--"particles/units/heroes/hero_tidehunter/tidehunter_gush_splash_water4c.vpcf",
+		--"particles/units/heroes/hero_mirana/mirana_spell_arrow.vpcf",
+		Source = caster,
+		vSpawnOrigin = attackPoint,
+		vVelocity = caster:GetForwardVector() * wave_speed,
+		fDistance = radius,
+		--fExpireTime = GameRules:GetGameTime() + ti,
+		--fMoveSpeed = wave_speed,
+		fStartRadius = wave_width,
+		fEndRadius = wave_width,
+		bHasFrontalCone = true,
+		iUnitTargetTeam = DOTA_UNIT_TARGET_TEAM_ENEMY,
+		iUnitTargetType = DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC,
+		--iUnitTargetFlags = DOTA_UNIT_TARGET_FLAG_NONE,
+		bProvidesVision = true,
+		fVisionRadius = vision_aoe,
+		--iVisionTeamNumber = caster:GetTeamNumber(),
+		--bReplaceExisting = false,
+		--bDeleteOnHit = true,
+    }
+    ProjectileManager:CreateLinearProjectile(info)
+end
