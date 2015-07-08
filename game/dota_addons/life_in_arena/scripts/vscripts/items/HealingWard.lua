9394b953a6267402137f5056633b21d2e980a7e4
ZLOY5 <ZLOY5@ukr.net>
2015-07-08 13:16:29 +0300

    Исправил блок от вардов

---
+++ game/dota_addons/life_in_arena/scripts/vscripts/items/HealingWard.lua
@@ -0,0 +1,10 @@
+function SpawnWard(event)
+	--local point = event.target_points[1]
+	
+	local dummy = CreateUnitByName("npc_dummy_blank", event.ability:GetCursorPosition(), true, event.caster, nil, event.caster:GetTeam())
+	
+	local ward = CreateUnitByName("item_lia_healing_ward_unit", dummy:GetAbsOrigin(), false, event.caster, nil, event.caster:GetTeam())
+	ward:AddNewModifier(ward, nil, "modifier_kill", {duratioon = 30})
+	
+	dummy:RemoveSelf()
+end
