33b5f256b0d4e58babed329c97bb09b34938bfc3
ZLOY5 <ZLOY5@ukr.net>
2015-07-13 14:27:47 +0300

    rtr

--- game/dota_addons/life_in_arena/scripts/vscripts/heroes/EarthLord/StoneSkin.lua
+++ game/dota_addons/life_in_arena/scripts/vscripts/heroes/EarthLord/StoneSkin.lua
@@ -2,10 +2,10 @@ function OnAttacked(event)
 	local target = event.target
 	local attacker = event.attacker
 	local ability = event.ability
-
-	if RollPercentage(ability:GetSpecialValueFor("chance"))
+	if RollPercentage(ability:GetSpecialValueFor("chance")) then 
 		if attacker:IsRangedAttacker() then 
-			ability:ApplyDataDrivenModifier(target, target, "modifier_earth_lord_stone_skin_ranged_no_damage", nil)
+			--ability:ApplyDataDrivenModifier(target, target, "modifier_earth_lord_stone_skin_ranged_no_damage", nil)
+			target:Heal(event.attack_damage,target)
 		else 
 			ability:ApplyDataDrivenModifier(target, attacker, "modifier_earth_lord_stone_skin_melee_damage_reduction", nil)
 		end
