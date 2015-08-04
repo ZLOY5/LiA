2ec1eb4384cc810eb997d5cc5f2bb9651a976169
ZLOY5 <ZLOY5@ukr.net>
2015-08-04 16:51:35 +0300

    Помощь

---
+++ game/dota_addons/life_in_arena/scripts/vscripts/heroes/Beastmaster/beastmaster_help.lua
@@ -0,0 +1,6 @@
+beastmaster_help = class({})
+LinkLuaModifier("modifier_beastmaster_help", "heroes/Beastmaster/modifier_beastmaster_help.lua", LUA_MODIFIER_MOTION_NONE)
+
+function beastmaster_help:GetIntrinsicModifierName() 
+	return "modifier_beastmaster_help"
+end
