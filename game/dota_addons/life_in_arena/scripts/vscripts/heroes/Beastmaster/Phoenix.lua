31666431ecbc773a8004f2521361c9d0adbe6a4b
MooMooKhan <korshunoffegor@yandex.ru>
2015-08-05 21:00:56 +0300

    говна

--- game/dota_addons/life_in_arena/scripts/vscripts/heroes/Beastmaster/Phoenix.lua
+++ game/dota_addons/life_in_arena/scripts/vscripts/heroes/Beastmaster/Phoenix.lua
@@ -1,10 +1,20 @@
 function SpawnEgg(event)
-	--local point = event.target_points[1]
-	
-	local dummy = CreateUnitByName("npc_dummy_blank", event.caster:GetAbsOrigin(), true, event.caster, nil, event.caster:GetTeam())
-	
-	local egg = CreateUnitByName("phoenix_egg_bm", dummy:GetAbsOrigin(), false, event.caster, nil, event.caster:GetTeam())
-	
-	dummy:RemoveSelf()
+	local hero = event.caster:GetOwnerEntity()
+	event.caster:AddNoDraw()
+	if  IsWave == true then
+		local dummy = CreateUnitByName("npc_dummy_blank", event.caster:GetAbsOrigin(), true, event.caster, nil, event.caster:GetTeam())
+		local egg = CreateUnitByName("phoenix_egg_bm", dummy:GetAbsOrigin(), false, hero, hero, event.caster:GetTeam())
+		egg:SetControllableByPlayer(hero:GetPlayerID(), true)
+		dummy:RemoveSelf()
+	end
 end
 
+function SpawnPhoenix(event)
+	local hero = event.caster:GetOwnerEntity()
+	if  IsWave == true then
+		event.caster:AddNoDraw()
+		local phoenix = CreateUnitByName("phoenix_bm", event.caster:GetAbsOrigin(), true, hero, hero, event.caster:GetTeam())	
+		phoenix:SetControllableByPlayer(hero:GetPlayerID(), true)
+		event.caster:Kill(event.ability, event.caster)
+	end
+end
