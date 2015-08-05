c5853e2caa794360041058f77885eafbd5702220
MooMooKhan <korshunoffegor@yandex.ru>
2015-08-05 22:13:20 +0300

    пзпззп

--- game/dota_addons/life_in_arena/scripts/vscripts/heroes/Beastmaster/Dominate.lua
+++ game/dota_addons/life_in_arena/scripts/vscripts/heroes/Beastmaster/Dominate.lua
@@ -8,6 +8,7 @@ function Dominate(keys)
 				bmcreep = CreateUnitByName(tostring(WAVE_NUM).."_bm", location, true, keys.caster, keys.caster, keys.caster:GetTeamNumber())
 				else bmcreep = CreateUnitByName(name, location, true, keys.caster, keys.caster, keys.caster:GetTeamNumber())
 			end
+			bmcreep:AddNoDraw()
 			bmcreep:SetControllableByPlayer(keys.caster:GetPlayerID(), true)
 			v:Kill(keys.ability, keys.caster)
 			else ApplyDamage({victim = v, attacker = keys.caster, damage = keys.ability:GetLevelSpecialValueFor("damage", keys.ability:GetLevel() - 1 ), damage_type = DAMAGE_TYPE_MAGICAL, ability = keys.ablility})
