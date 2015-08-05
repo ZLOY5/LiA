9a9c01b45649b408fb78c394d84cdbc380e24534
MooMooKhan <korshunoffegor@yandex.ru>
2015-08-05 17:10:47 +0300

    урон по непризываемым и вернул реген

--- game/dota_addons/life_in_arena/scripts/vscripts/heroes/Beastmaster/Dominate.lua
+++ game/dota_addons/life_in_arena/scripts/vscripts/heroes/Beastmaster/Dominate.lua
@@ -1,7 +1,7 @@
 function Dominate(keys)
 	local targets = keys.target_entities
 	for _,v in pairs(targets) do
-		if not string.find(v:GetUnitName(),"boss") then --проверяем не босс или не мегабосс ли юнит
+		if not string.find(v:GetUnitName(),"boss") and not v:IsHero() then --проверяем не босс или не мегабосс ли юнит
 			local name = v:GetUnitName()
 			local location = v:GetAbsOrigin()
 			if string.find(v:GetUnitName(),"wave_creep") then
@@ -10,6 +10,7 @@ function Dominate(keys)
 			end
 			bmcreep:SetControllableByPlayer(keys.caster:GetPlayerID(), true)
 			v:Kill(keys.ability, keys.caster)
+			else ApplyDamage({victim = v, attacker = keys.caster, damage = keys.ability:GetLevelSpecialValueFor("damage", keys.ability:GetLevel() - 1 ), damage_type = DAMAGE_TYPE_MAGICAL, ability = keys.ablility})
 		end
 	end 
 end
