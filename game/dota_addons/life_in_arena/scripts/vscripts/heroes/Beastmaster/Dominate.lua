7e12d3794968873d6668843e8a28c19aec508eb8
MooMooKhan <korshunoffegor@yandex.ru>
2015-08-07 14:24:16 +0300

    еще вещи

--- game/dota_addons/life_in_arena/scripts/vscripts/heroes/Beastmaster/Dominate.lua
+++ game/dota_addons/life_in_arena/scripts/vscripts/heroes/Beastmaster/Dominate.lua
@@ -5,16 +5,14 @@ function Dominate(keys)
 			local name = v:GetUnitName()
 			local location = v:GetAbsOrigin()
 			local predeath_hp = v:GetHealth()
-			v:AddNoDraw()
-			v:Kill(keys.ability, keys.caster)
 			if string.find(v:GetUnitName(),"wave_creep") then
-				if  IsWave == true then
-					bmcreep = CreateUnitByName(tostring(WAVE_NUM).."_bm", location, true, keys.caster, keys.caster, keys.caster:GetTeamNumber())
-					else bmcreep = CreateUnitByName(name, location, true, keys.caster, keys.caster, keys.caster:GetTeamNumber())
-				end
+					bmcreep = CreateUnitByName(tostring(WAVE_NUM).."_bm", location, false, keys.caster, keys.caster, keys.caster:GetTeamNumber())
+					else bmcreep = CreateUnitByName(name, location, false, keys.caster, keys.caster, keys.caster:GetTeamNumber())
 			end
+			v:AddNoDraw()
 			bmcreep:SetHealth(predeath_hp)
 			bmcreep:SetControllableByPlayer(keys.caster:GetPlayerID(), true)
+			v:Kill(keys.ability, keys.caster)
 			else ApplyDamage({victim = v, attacker = keys.caster, damage = keys.ability:GetLevelSpecialValueFor("damage", keys.ability:GetLevel() - 1 ), damage_type = DAMAGE_TYPE_MAGICAL, ability = keys.ablility})
 		end
 	end 
