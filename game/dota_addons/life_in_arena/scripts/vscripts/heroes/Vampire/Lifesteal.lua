c012eecdebc28ccb276403a0fa35306ce09b441d
MooMooKhan <korshunoffegor@yandex.ru>
2015-08-08 12:12:22 +0300

    вампиризм

---
+++ game/dota_addons/life_in_arena/scripts/vscripts/heroes/Vampire/Lifesteal.lua
@@ -0,0 +1,20 @@
+function modifier_vampire_lifesteal_lifesteal(keys)
+	if --[[keys.target:GetInvulnCount() == nil and]] not keys.target:IsMechanical() and keys.target:GetTeamNumber() ~= keys.caster:GetTeamNumber() then
+		keys.ability:ApplyDataDrivenModifier(keys.attacker, keys.attacker, "modifier_vampire_lifesteal_lifesteal", {duration = 0.03})
+	end
+end
+
+function thirst_points(event)
+	if event.ability:GetLevel() == 1 then
+		thirst_points = 0
+	end
+	event.caster:SetModifierStackCount("modifier_vampire_lifesteal", event.caster, thirst_points)
+end
+
+function ThirstPointsGain(event)
+	thirst_points_max = event.ability:GetLevelSpecialValueFor("thirst_points_max", event.ability:GetLevel() - 1)
+	if thirst_points < thirst_points_max then
+		thirst_points = thirst_points + 1
+		event.caster:SetModifierStackCount("modifier_vampire_lifesteal", event.caster, thirst_points)
+	end
+end
