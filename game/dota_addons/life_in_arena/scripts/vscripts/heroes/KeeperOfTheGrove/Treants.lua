f3815d1a880e2e2ba5821c83839055eb9cef5646
MooMooKhan <korshunoffegor@yandex.ru>
2015-08-13 15:17:58 +0300

    еще немного рощи

---
+++ game/dota_addons/life_in_arena/scripts/vscripts/heroes/KeeperOfTheGrove/Treants.lua
@@ -0,0 +1,13 @@
+function SummonTreants(event)
+	local point = event.target_points[1]
+	local ability = event.ability
+	local ability_level = ability:GetLevel()
+	local unit_name = "keeper_of_the_grove_treant"..ability_level
+	local count = 2
+
+
+	for i=1,count do
+		kotg_treant = CreateUnitByName(unit_name, point, true, event.caster, event.caster, event.caster:GetTeam())
+		kotg_treant:SetControllableByPlayer(event.caster:GetPlayerID(), true)
+	end
+end
