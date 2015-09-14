51b7dd18fd0f3d1c6c03d799eb1276953bcbbc23
MooMooKhan <korshunoffegor@yandex.ru>
2015-09-14 21:34:56 +0300

    в

---
+++ game/dota_addons/life_in_arena/scripts/vscripts/abilities/GhostSlow.lua
@@ -0,0 +1,11 @@
+function Slow(event)
+	local ability = event.ability
+	local caster = event.caster
+	local target = event.target
+
+	if target:IsHero() then 
+		ability:ApplyDataDrivenModifier(caster, target, "modifier_16_wave_slow", {duration = hero_duration})	
+	else
+		ability:ApplyDataDrivenModifier(caster, target, "modifier_16_wave_slow", {duration = duration})
+	end
+end
