e7bb795af6608c797cbd0d9fd1b63c190c686af2
cezarrr666 <666rim@mail.ru>
2015-09-15 22:28:56 +0500

    третий мегабосс + фиксы разные

--- game/dota_addons/life_in_arena/scripts/vscripts/AI/15_megaboss.lua
+++ game/dota_addons/life_in_arena/scripts/vscripts/AI/15_megaboss.lua
@@ -6,6 +6,7 @@ function Spawn(entityKeyValues)
 	end
 	
 	ABILITY_15_megaboss_illusions = thisEntity:FindAbilityByName("15_megaboss_illusions")
+	ABILITY_15_megaboss_astral = thisEntity:FindAbilityByName("15_megaboss_astral")
 	thisEntity:SetContextThink( "15_megaboss_think", Think15Wave , 2)
 end
 
@@ -20,7 +21,30 @@ function Think15Wave()
 
 	if ABILITY_15_megaboss_illusions:IsFullyCastable() and thisEntity:GetHealthPercent() < 80 then
 		thisEntity:CastAbilityNoTarget(ABILITY_15_megaboss_illusions, -1)
-	end	
+	else
+		if ABILITY_15_megaboss_astral:IsFullyCastable() then
+			local targets = FindUnitsInRadius(thisEntity:GetTeam(), 
+							  thisEntity:GetOrigin(), 
+							  nil, 
+							  800, 
+							  DOTA_UNIT_TARGET_TEAM_ENEMY, 
+							  DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC, 
+							  DOTA_UNIT_TARGET_FLAG_NONE, 
+							  FIND_ANY_ORDER, 
+							  false)
+			if #targets ~= 0 then
+				local targetTo = nil
+				for i=1,#targets do
+					if not targets[i]:HasModifier("modifier_decrepify_hero") and not targets[i]:HasModifier("mod_15_megaboss_silence") then
+						targetTo = targets[i]
+					end
+				end
+				if targetTo ~= nil then
+					thisEntity:CastAbilityOnTarget(targetTo, ABILITY_15_megaboss_astral, -1)
+				end
+			end
+		end
+	end
 	
 	return 2
 end
