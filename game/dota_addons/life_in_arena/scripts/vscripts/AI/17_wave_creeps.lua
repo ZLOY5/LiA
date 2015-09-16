3e1cab099d5a6ad16596035684709088ced681b8
cezarrr666 <666rim@mail.ru>
2015-09-16 20:56:02 +0500

    1

---
+++ game/dota_addons/life_in_arena/scripts/vscripts/AI/17_wave_creeps.lua
@@ -0,0 +1,49 @@
+require('survival/AIcreeps')
+
+function Spawn(entityKeyValues)
+	--print("Spawn")
+    thisEntity:SetHullRadius(32) 
+	if thisEntity:GetPlayerOwnerID() ~= -1 then
+		return
+	end
+	
+	ABILITY_17_wave_purge = thisEntity:FindAbilityByName("17_wave_purge")
+	thisEntity:SetContextThink( "AIThink", AIThink , 0.1)
+end
+
+function AIThink()
+	if not thisEntity:IsAlive() then
+		return nil 
+	end
+
+	if GameRules:IsGamePaused() then
+		return 1
+	end
+
+	AICreepsAttackOneUnit({unit = thisEntity})
+	--print(LiA.AICreepCasts)
+		
+	if ABILITY_17_wave_purge:IsFullyCastable() and Survival.AICreepCasts < Survival.AIMaxCreepCasts then
+		local targets = FindUnitsInRadius(thisEntity:GetTeam(), 
+						  thisEntity:GetOrigin(), 
+						  nil, 
+						  700, 
+						  DOTA_UNIT_TARGET_TEAM_ENEMY, 
+						  DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC, 
+						  DOTA_UNIT_TARGET_FLAG_MANA_ONLY, 
+						  FIND_ANY_ORDER, 
+						  false)
+		--print(#targets)
+		for k,unit in pairs(targets) do 
+			if unit:HasModifier("modifier_purge_slow") then 
+				table.remove(targets,k)
+			end 
+		end 
+		
+		if #targets ~= 0 then
+			thisEntity:CastAbilityOnTarget(targets[RandomInt(1,#targets)], ABILITY_17_wave_purge, -1)
+			Survival.AICreepCasts = Survival.AICreepCasts + 1
+		end
+	end
+	return 2
+end
