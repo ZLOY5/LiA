723b8bdbac034ea55372b4cc4076c5f114079b59
ZLOY5 <ZLOY5@ukr.net>
2015-09-26 16:50:05 +0300

    Мифрил и Мегабосс

--- game/dota_addons/life_in_arena/scripts/vscripts/items/mithril_armor.lua
+++ game/dota_addons/life_in_arena/scripts/vscripts/items/mithril_armor.lua
@@ -31,4 +31,5 @@ function TauntEnd( keys )
 	local target = keys.target
 
 	target:SetForceAttackTarget(nil)
+	target:MoveToPositionAggressive(target:GetAbsOrigin()+target:GetForwardVector()*50)
 end
