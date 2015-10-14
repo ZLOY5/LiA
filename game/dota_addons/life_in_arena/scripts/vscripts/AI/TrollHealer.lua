98f62682fab78068fad023b4125b4fa35e5bed3b
ZLOY5 <ZLOY5@ukr.net>
2015-10-14 15:15:49 +0300

    Автокаст тролля

---
+++ game/dota_addons/life_in_arena/scripts/vscripts/AI/TrollHealer.lua
@@ -0,0 +1,41 @@
+function Spawn(entityKeyValues)
+
+	ABILITY_Heal = thisEntity:FindAbilityByName("troll_healer_heal")
+	ABILITY_Heal:ToggleAutoCast()
+	thisEntity:SetContextThink( "AutocastAI", AutocastAIThink , 0.1)
+end
+
+function AutocastAIThink()
+	if not thisEntity:IsAlive() then
+		return nil 
+	end
+
+	if GameRules:IsGamePaused() then
+		return 0.1
+	end
+
+	local target
+
+	if ABILITY_Heal:GetAutoCastState() and ABILITY_Heal:IsFullyCastable() then 
+		local targets = FindUnitsInRadius( thisEntity:GetTeamNumber(), thisEntity:GetAbsOrigin(), nil, ABILITY_Heal:GetCastRange(), DOTA_UNIT_TARGET_TEAM_FRIENDLY, DOTA_UNIT_TARGET_BASIC+DOTA_UNIT_TARGET_HERO, DOTA_UNIT_TARGET_FLAG_NONE, FIND_ANY_ORDER, false )
+
+		for k,unit in pairs(targets) do 
+			if unit:GetHealthPercent() ~= 100 then 
+				if target == nil or target:GetHealthPercent() > unit:GetHealthPercent() then
+					target = unit 
+				end
+			end
+		end
+
+		if target then 
+			ExecuteOrderFromTable({
+ 				UnitIndex = thisEntity:entindex(), 
+ 				OrderType = DOTA_UNIT_ORDER_CAST_TARGET,
+ 		 		TargetIndex = target:entindex(), 
+ 				AbilityIndex = ABILITY_Heal:entindex(), 
+ 			})
+		end
+	end
+
+	return 0.5
+end
