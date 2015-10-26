2e04e57946cacb73558d6abd01da713dd4cc9aa6
ZLOY5 <ZLOY5@ukr.net>
2015-10-26 09:59:26 +0200

    Фикс Силы Плети

--- game/dota_addons/life_in_arena/scripts/vscripts/heroes/Araman/modifier_araman_power_of_scourge.lua
+++ game/dota_addons/life_in_arena/scripts/vscripts/heroes/Araman/modifier_araman_power_of_scourge.lua
@@ -22,11 +22,16 @@ end
 function modifier_araman_power_of_scourge:DeclareFunctions()
 	local funcs = {
 		MODIFIER_PROPERTY_ATTACKSPEED_BONUS_CONSTANT,
+		MODIFIER_PROPERTY_MOVESPEED_BONUS_PERCENTAGE,
 		MODIFIER_PROPERTY_PREATTACK_BONUS_DAMAGE,
 		MODIFIER_EVENT_ON_ATTACK_LANDED,
 	}
  
 	return funcs
+end
+
+function modifier_araman_power_of_scourge:GetModifierMoveSpeedBonus_Percentage(params)
+	return self.bonus_move_speed
 end
 
 function modifier_araman_power_of_scourge:GetModifierAttackSpeedBonus_Constant(params)
@@ -45,11 +50,12 @@ function modifier_araman_power_of_scourge:OnCreated(params)
 	self.bonus_damage = 0
 
 	self.bonus_attack_speed = self:GetAbility():GetSpecialValueFor("bonus_attack_speed")
+	self.bonus_move_speed = self:GetAbility():GetSpecialValueFor("bonus_move_speed")
 end
 
 function modifier_araman_power_of_scourge:OnAttackLanded(params)
 	if IsServer() then
-		if params.attacker == self:GetParent() and not self:GetParent():IsIllusion() and params.target:IsOpposingTeam(params.attacker:GetTeamNumber()) then
+		if params.attacker == self:GetParent() and not self:GetParent():IsIllusion() and params.target:GetTeamNumber() ~= params.attacker:GetTeamNumber() then
 			if self.power_of_scourge_stacks < self.power_of_scourge_max_stacks then
 				self.bonus_damage = self.bonus_damage + self.damage_per_stack
 				self.power_of_scourge_stacks = self.power_of_scourge_stacks + 1
