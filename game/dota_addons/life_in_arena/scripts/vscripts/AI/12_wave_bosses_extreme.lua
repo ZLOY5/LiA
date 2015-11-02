7c25c7cfa3abae7bc188ef83cd023855991fe49c
cezarrr666 <666rim@mail.ru>
2015-11-02 20:27:04 +0500

    Экстрим: Боссы, волны 11-19

---
+++ game/dota_addons/life_in_arena/scripts/vscripts/AI/12_wave_bosses_extreme.lua
@@ -0,0 +1,60 @@
+require('survival/AIcreeps')
+
+function Spawn(entityKeyValues)
+	--print("Spawn")
+    thisEntity:SetHullRadius(32) 
+	if thisEntity:GetPlayerOwnerID() ~= -1 then
+		return
+	end
+	
+	ABILITY_12_wave_bloodlust = thisEntity:FindAbilityByName("12_wave_bloodlust_extreme")
+	ABILITY_12_wave_roots = thisEntity:FindAbilityByName("12_wave_roots_extreme")
+	thisEntity:SetContextThink( "12_wave_think", Think12Wave , 0.1)
+end
+
+function Think12Wave()
+	if not thisEntity:IsAlive() then
+		return nil 
+	end
+
+	if GameRules:IsGamePaused() then
+		return 1
+	end
+
+	AICreepsAttackOneUnit({unit = thisEntity})
+	--print(LiA.AICreepCasts)
+		
+	if ABILITY_12_wave_bloodlust:IsFullyCastable() and not thisEntity:IsStunned() and Survival.AICreepCasts < Survival.AIMaxCreepCasts then
+		if thisEntity:GetHealthPercent() >= 25 then
+			thisEntity:CastAbilityNoTarget(ABILITY_12_wave_bloodlust, -1)
+			Survival.AICreepCasts = Survival.AICreepCasts + 1
+		end
+	else
+		if ABILITY_12_wave_roots:IsFullyCastable() and not thisEntity:IsStunned() and Survival.AICreepCasts < Survival.AIMaxCreepCasts then
+			local targets = FindUnitsInRadius(thisEntity:GetTeam(), 
+							  thisEntity:GetOrigin(), 
+							  nil, 
+							  600, 
+							  DOTA_UNIT_TARGET_TEAM_ENEMY, 
+							  DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC, 
+							  DOTA_UNIT_TARGET_FLAG_MANA_ONLY, 
+							  FIND_ANY_ORDER, 
+							  false)
+			for k,unit in pairs(targets) do 
+				if unit:HasModifier("modifier_entangling_roots") or unit:IsStunned() then 
+					table.remove(targets,k)
+				end 
+			end 
+			
+			if #targets ~= 0 then
+				thisEntity:CastAbilityOnTarget(targets[RandomInt(1,#targets)], ABILITY_12_wave_roots, -1)
+				Survival.AICreepCasts = Survival.AICreepCasts + 1
+			end
+		
+		end
+	
+	end	
+	
+	
+	return 2
+end
---
+++ game/dota_addons/life_in_arena/scripts/vscripts/AI/16_wave_bosses_extreme.lua
@@ -0,0 +1,76 @@
+require('survival/AIcreeps')
+
+function Spawn(entityKeyValues)
+	--print("Spawn")
+    thisEntity:SetHullRadius(32) 
+	if thisEntity:GetPlayerOwnerID() ~= -1 then
+		return
+	end
+	
+	ABILITY_16_wave_mana_burn = thisEntity:FindAbilityByName("16_wave_mana_burn_extreme")
+	ABILITY_16_wave_slow = thisEntity:FindAbilityByName("16_wave_slow_extreme")
+	thisEntity:SetContextThink( "AIThink", AIThink , 0.1)
+end
+
+function AIThink()
+	if not thisEntity:IsAlive() then
+		return nil 
+	end
+
+	if GameRules:IsGamePaused() then
+		return 1
+	end
+
+	AICreepsAttackOneUnit({unit = thisEntity})
+	--print(Survival.AICreepCasts)
+
+	if thisEntity:IsStunned() then 
+		return 2 
+	end
+
+	if ABILITY_16_wave_slow:IsFullyCastable() then --в первую очередь босс кастует замедление
+		local targets = FindUnitsInRadius(thisEntity:GetTeam(), 
+						  thisEntity:GetOrigin(), 
+						  nil, 
+						  700, 
+						  DOTA_UNIT_TARGET_TEAM_ENEMY, 
+						  DOTA_UNIT_TARGET_HERO, 
+						  DOTA_UNIT_TARGET_FLAG_NONE, 
+						  FIND_ANY_ORDER, 
+						  false)
+
+		--не накладываем заклинание на тех, кто уже под его действием
+		for k,target in pairs(targets) do 
+			if target:HasModifier("modifier_16_wave_slow") then 
+				table.remove(targets,k)
+			end
+		end
+		if #targets ~= 0 then
+			thisEntity:CastAbilityOnTarget(targets[RandomInt(1,#targets)], ABILITY_16_wave_slow, -1)
+			Survival.AICreepCasts = Survival.AICreepCasts + 1
+			return 2
+		end
+	end
+
+	if Survival.AICreepCasts >= Survival.AIMaxCreepCasts then --мана берн не кастуем, если превышен лимит кастов
+		return 2
+	end
+
+	if ABILITY_16_wave_mana_burn:IsFullyCastable() then
+		local targets = FindUnitsInRadius(thisEntity:GetTeam(), 
+						  thisEntity:GetOrigin(), 
+						  nil, 
+						  500, 
+						  DOTA_UNIT_TARGET_TEAM_ENEMY, 
+						  DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC, 
+						  DOTA_UNIT_TARGET_FLAG_MANA_ONLY, 
+						  FIND_ANY_ORDER, 
+						  false)
+		if #targets ~= 0 then
+			thisEntity:CastAbilityOnTarget(targets[RandomInt(1,#targets)], ABILITY_16_wave_mana_burn, -1)
+			Survival.AICreepCasts = Survival.AICreepCasts + 1
+		end
+	end	
+	
+	return 2
+end
