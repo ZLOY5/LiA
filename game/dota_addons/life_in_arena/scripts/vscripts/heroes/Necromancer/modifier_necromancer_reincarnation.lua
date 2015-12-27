41fd47bf10601135d69936c70356223030165224
ZLOY5 <ZLOY5@ukr.net>
2015-12-27 14:24:20 +0200

    и еще

--- game/dota_addons/life_in_arena/scripts/vscripts/heroes/Necromancer/modifier_necromancer_reincarnation.lua
+++ game/dota_addons/life_in_arena/scripts/vscripts/heroes/Necromancer/modifier_necromancer_reincarnation.lua
@@ -20,11 +20,37 @@ function modifier_necromancer_reincarnation:ReincarnateTime(params)
 	--print("ReincarnateTime")
 	if self:GetAbility():IsCooldownReady() then
 		self:GetAbility():StartCooldown(self:GetAbility():GetCooldown(self:GetAbility():GetLevel()-1))
+
+		local parent = self:GetParent()
+		local respawnPosition = parent:GetAbsOrigin()
+
+		local particleName = "particles/units/heroes/hero_skeletonking/wraith_king_reincarnate.vpcf"
+		parent.ReincarnateParticle = ParticleManager:CreateParticle( particleName, PATTACH_ABSORIGIN_FOLLOW, parent )
+		ParticleManager:SetParticleControl(parent.ReincarnateParticle, 0, respawnPosition)
+		ParticleManager:SetParticleControl(parent.ReincarnateParticle, 1, Vector(500,0,0))
+		ParticleManager:SetParticleControl(parent.ReincarnateParticle, 1, Vector(500,500,0))
+
+		local particleName = "particles/units/heroes/hero_skeletonking/skeleton_king_death_bits.vpcf"
+		local particle1 = ParticleManager:CreateParticle( particleName, PATTACH_ABSORIGIN, parent )
+		ParticleManager:SetParticleControl(particle1, 0, respawnPosition)
+
+		local particleName = "particles/units/heroes/hero_skeletonking/skeleton_king_death_dust.vpcf"
+		local particle2 = ParticleManager:CreateParticle( particleName, PATTACH_ABSORIGIN_FOLLOW, parent )
+		ParticleManager:SetParticleControl(particle2, 0, respawnPosition)
+
+		local particleName = "particles/units/heroes/hero_skeletonking/skeleton_king_death_dust_reincarnate.vpcf"
+		local particle3 = ParticleManager:CreateParticle( particleName, PATTACH_ABSORIGIN_FOLLOW, parent )
+		ParticleManager:SetParticleControl(particle3 , 0, respawnPosition)
+
+		parent:EmitSound("Hero_SkeletonKing.Reincarnate")
+		parent:EmitSound("Hero_SkeletonKing.Death")
 		
 		Timers:CreateTimer(self.reincarnateTime,
 			function() 
-				ParticleManager:CreateParticle("particles/units/heroes/hero_skeletonking/wraith_king_reincarnate.vpcf", PATTACH_ABSORIGIN, self:GetParent())
-				self:GetParent():RespawnHero(false, true, true)
+				ParticleManager:DestroyParticle(parent.ReincarnateParticle, false)
+				parent:EmitSound("Hero_SkeletonKing.Reincarnate.Stinger")
+				
+				self:GetParent():RespawnHero(false, false, false)
 			end
 		)
 		return self.reincarnateTime
