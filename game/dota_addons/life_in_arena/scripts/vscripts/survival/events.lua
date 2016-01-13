b450e36671f70bdf56122228e6983ceab6c55228
MooMooKhan <korshunoffegor@yandex.ru>
2016-01-13 21:04:42 +0300

    d

--- game/dota_addons/life_in_arena/scripts/vscripts/survival/events.lua
+++ game/dota_addons/life_in_arena/scripts/vscripts/survival/events.lua
@@ -162,7 +162,7 @@ end
 
 function Survival:OnGameStateChange()
     if GameRules:State_Get() == DOTA_GAMERULES_STATE_GAME_IN_PROGRESS then
-        self.nRoundNum = 7
+      --  self.nRoundNum = 3
         GameRules:SetPreGameTime(120)
         Survival:PrepareNextRound()
     end
