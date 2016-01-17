cc188301543faff4c33926edc9f554b192231f7b
ZLOY5 <ZLOY5@ukr.net>
2016-01-17 22:11:41 +0200

    Фикс Огромнго топора и КЛ

--- game/dota_addons/life_in_arena/scripts/vscripts/items/cleave_axes.lua
+++ game/dota_addons/life_in_arena/scripts/vscripts/items/cleave_axes.lua
@@ -24,14 +24,14 @@ function OnEquipBloodMoon(event)
 end
 
 function OnUnequip(event)
-	if not event.caster:HasItemInInventory("item_lia_huge_axe") then
+	if not event.caster:HasModifier("modifier_item_lia_huge_axe") then
 		event.caster:RemoveModifierByName("item_huge_axe_cleave_modifier")
 		print("Huge Axe modifier removed")
 	end
-	if not event.caster:HasItemInInventory("item_lia_blood_moon") then
+	if not event.caster:HasModifier("modifier_item_blood_moon") then
 		event.caster:RemoveModifierByName("item_blood_moon_cleave_modifier")
 		print("Blood Moon modifier removed")
-		if event.caster:HasItemInInventory("item_lia_huge_axe") then
+		if event.caster:HasModifier("modifier_item_lia_huge_axe") then
 			local item = GetItemInInventory(event.caster,"item_lia_huge_axe")
 			item:ApplyDataDrivenModifier(event.caster, event.caster, "item_huge_axe_cleave_modifier", nil) 	
 			print("Huge Axe modifier added")
