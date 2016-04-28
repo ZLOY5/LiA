1eb8e11cc2aec72e88f03601c10372f476dee8e8
ZLOY5 <ZLOY5@ukr.net>
2016-04-28 21:09:51 +0300

    3 скилл Голема
    1

--- game/dota_addons/life_in_arena/scripts/vscripts/heroes/Acron/firerage.lua
+++ game/dota_addons/life_in_arena/scripts/vscripts/heroes/Acron/firerage.lua
@@ -1,22 +1,37 @@
 function StackCountIncrease(keys)
     local caster = keys.caster
     local ability = keys.ability
-    local currentStacks = caster:GetModifierStackCount("modifier_firerage_datadriven", caster)
+    local currentStacks = caster:GetModifierStackCount("modifier_firerage_datadriven", caster) + 1
     local maxStack = keys.Stack
+    local stackTime = ability:GetSpecialValueFor("stackTime")
     
-    currentStacks = currentStacks + 1
+    local attackGameTime = GameRules:GetGameTime()
+    
+    ability.lastExplosionTime = ability.lastExplosionTime or 0
 
-    keys.caster:SetModifierStackCount("modifier_firerage_datadriven", keys.ability, currentStacks) 
+    if keys.attacker:IsRealHero() or string.find(keys.attacker:GetUnitName(),"megaboss") then
+        currentStacks = currentStacks+1
+    end
+
+    caster:SetModifierStackCount("modifier_firerage_datadriven", ability, currentStacks) 
 
     if currentStacks == maxStack then 
-        keys.caster:SetModifierStackCount("modifier_firerage_datadriven", keys.ability, 0) 
+        --print("boom")
+        ability.lastExplosionTime = GameRules:GetGameTime()
+        caster:SetModifierStackCount("modifier_firerage_datadriven", ability, 0) 
         ability:ApplyDataDrivenModifier( caster, caster, "modifier_firerage_explosion", {duration = 0.03})
     end
-end
 
-function StackCountDecrease(keys)
-    local currentStacks = caster:GetModifierStackCount("modifier_firerage_datadriven", caster)
-    if currentStacks ~= 0 then
-        keys.caster:SetModifierStackCount("modifier_firerage_datadriven", keys.ability, currentStacks-1) 
-    end
+    Timers:CreateTimer(stackTime,
+        function() 
+            if attackGameTime > ability.lastExplosionTime then
+                local currentStacks = caster:GetModifierStackCount("modifier_firerage_datadriven", caster)
+                if currentStacks ~= 0 then
+                    caster:SetModifierStackCount("modifier_firerage_datadriven", ability, currentStacks-1)
+                end
+            else
+                --print("expiredStack")
+            end
+        end
+    )
 end
