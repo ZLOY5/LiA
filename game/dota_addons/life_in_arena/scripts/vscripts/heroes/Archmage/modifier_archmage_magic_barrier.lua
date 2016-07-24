1a947b381d05b615b64380818e07daf3a8451dd2
ZLOY5 <ZLOY5@ukr.net>
2016-07-24 20:46:46 +0300

    Архимаг
    Фикс тултипа

--- game/dota_addons/life_in_arena/scripts/vscripts/heroes/Archmage/modifier_archmage_magic_barrier.lua
+++ game/dota_addons/life_in_arena/scripts/vscripts/heroes/Archmage/modifier_archmage_magic_barrier.lua
@@ -67,7 +67,7 @@ function modifier_archmage_magic_barrier:OnAbilityExecuted(params)
 
 		self:GetAbility().barrierMana = self:GetAbility().barrierMana + eventAbility:GetManaCost(-1)*self.barrierManaPercent
 		CustomNetTables:SetTableValue("custom_modifier_state",tostring(self:GetAbility():GetEntityIndex()),{ barrierMana = self:GetAbility().barrierMana })
-		print("Archmage[Mana barrier]: Added "..eventAbility:GetManaCost(-1)*self.barrierManaPercent.." mana",self:GetAbility().barrierMana)
+		--print("Archmage[Mana barrier]: Added "..eventAbility:GetManaCost(-1)*self.barrierManaPercent.." mana",self:GetAbility().barrierMana)
 	end
 end
 
@@ -81,22 +81,26 @@ function modifier_archmage_magic_barrier:OnIntervalThink()
 			ability.barrierMana = ability.barrierMana - self.barrierManaLoss
 		end
 
+		local parent = self:GetParent()
+
 		if ability.barrierMana > 0 then
 			local shield_size = 75
+			
+			if parent.magicBarrerParticle == nil then
+				local particle = ParticleManager:CreateParticle("particles/abaddon_aphotic_shield.vpcf", PATTACH_ABSORIGIN_FOLLOW, parent)
+			
+				ParticleManager:SetParticleControl(particle, 1, Vector(shield_size,0,shield_size))
+				ParticleManager:SetParticleControl(particle, 2, Vector(shield_size,0,shield_size))
+				ParticleManager:SetParticleControl(particle, 4, Vector(shield_size,0,shield_size))
+				ParticleManager:SetParticleControl(particle, 5, Vector(shield_size,0,0))
+				ParticleManager:SetParticleControlEnt(particle, 0, parent, PATTACH_POINT_FOLLOW, "attach_hitloc", parent:GetAbsOrigin(), true)
+				
+				parent.magicBarrerParticle = particle
 
-			if self:GetParent().magicBarrerParticle == nil then
-				self:GetParent().magicBarrerParticle = ParticleManager:CreateParticle("particles/abaddon_aphotic_shield.vpcf", PATTACH_ABSORIGIN_FOLLOW, self:GetParent())
-				ParticleManager:SetParticleControl(self:GetParent().magicBarrerParticle, 1, Vector(shield_size,0,shield_size))
-				ParticleManager:SetParticleControl(self:GetParent().magicBarrerParticle, 2, Vector(shield_size,0,shield_size))
-				ParticleManager:SetParticleControl(self:GetParent().magicBarrerParticle, 4, Vector(shield_size,0,shield_size))
-				ParticleManager:SetParticleControl(self:GetParent().magicBarrerParticle, 5, Vector(shield_size,0,0))
-
-				ParticleManager:SetParticleControlEnt(self:GetParent().magicBarrerParticle, 0, self:GetParent(), PATTACH_POINT_FOLLOW, "attach_hitloc", self:GetParent():GetAbsOrigin(), true)
-				--
 			end	
-		elseif ability.barrierMana == 0 and self:GetParent().magicBarrerParticle ~= nil then
-			ParticleManager:DestroyParticle(self:GetParent().magicBarrerParticle,true)
-			self:GetParent().magicBarrerParticle = nil
+		elseif ability.barrierMana == 0 and parent.magicBarrerParticle ~= nil then
+			ParticleManager:DestroyParticle(parent.magicBarrerParticle,true)
+			parent.magicBarrerParticle = nil
 		end
 
 		CustomNetTables:SetTableValue("custom_modifier_state", tostring( ability:GetEntityIndex() ) ,{ barrierMana = ability.barrierMana })
@@ -104,29 +108,29 @@ function modifier_archmage_magic_barrier:OnIntervalThink()
 		local netTable = CustomNetTables:GetTableValue("custom_modifier_state", tostring( ability:GetEntityIndex() ) )
 
 		if netTable then
-			ability.barrierMana = netTable.barrierMana
+			self.barrierMana = netTable.barrierMana
 		end
+		--print(ability.barrierMana,"client side")
 	end
 
 
 	return 0.5
 end
 
-if IsServer() then
 
-	function modifier_archmage_magic_barrier:OnDeath(params)
-		if params.unit == self:GetParent() then
-			self:GetAbility().target = nil
-			CustomNetTables:SetTableValue("custom_modifier_state", tostring( self:GetAbility():GetEntityIndex().."behavior" ), 
-				{ behavior = DOTA_ABILITY_BEHAVIOR_UNIT_TARGET } )
-			if self:GetCaster():IsAlive() then
-				self:GetCaster():AddNewModifier(self:GetCaster(),self:GetAbility(),"modifier_archmage_magic_barrier",nil)
-			end
+function modifier_archmage_magic_barrier:OnDeath(params)
+	if params.unit == self:GetParent() then
+		self:GetAbility().target = nil
+		CustomNetTables:SetTableValue("custom_modifier_state", tostring( self:GetAbility():GetEntityIndex().."behavior" ), 
+			{ behavior = DOTA_ABILITY_BEHAVIOR_UNIT_TARGET } )
+		if self:GetCaster():IsAlive() then
+			self:GetCaster():AddNewModifier(self:GetCaster(),self:GetAbility(),"modifier_archmage_magic_barrier",nil)
 		end
 	end
-
 end
 
-function modifier_archmage_magic_barrier:OnTooptip(params)
-	return ability.barrierMana
+
+
+function modifier_archmage_magic_barrier:OnTooltip(params)
+	return self.barrierMana
 end
