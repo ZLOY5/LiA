617d5d15288cca8653d84dcece06d8e76fa94dbf
ZLOY5 <ZLOY5@ukr.net>
2016-08-09 21:34:14 +0300

    Исправил партикл

--- game/dota_addons/life_in_arena/scripts/vscripts/items/modifier_item_lia_pure_light.lua
+++ game/dota_addons/life_in_arena/scripts/vscripts/items/modifier_item_lia_pure_light.lua
@@ -1,5 +1,5 @@
 modifier_item_lia_pure_light = class({})
-modifier_item_lia_pure_light_protection = class({})
+
 
 function modifier_item_lia_pure_light:IsHidden()
 	return false
@@ -31,15 +31,17 @@ end
 
 function modifier_item_lia_pure_light:OnIntervalThink()
 	if IsServer() then
-		local health_percent_threshold = self:GetAbility():GetSpecialValueFor("health_percent_threshold")
-		local totem_radius = self:GetAbility():GetSpecialValueFor("totem_radius")
-		local buff_duration = self:GetAbility():GetSpecialValueFor("buff_duration")
-		local totem_targets = self:GetAbility():GetSpecialValueFor("totem_targets")
+		local ability = self:GetAbility()
 		local caster = self:GetParent()
 
+		local health_percent_threshold = ability:GetSpecialValueFor("health_percent_threshold")
+		local totem_radius = ability:GetSpecialValueFor("totem_radius")
+		local buff_duration = ability:GetSpecialValueFor("buff_duration")
+		local totem_targets = ability:GetSpecialValueFor("totem_targets")
+		
 
-		local pureLightPossibleTargets = FindUnitsInRadius(self:GetParent():GetTeamNumber(), 
-																self:GetParent():GetAbsOrigin(), 
+		local pureLightPossibleTargets = FindUnitsInRadius(caster:GetTeamNumber(), 
+																caster:GetAbsOrigin(), 
 																nil, totem_radius, 
 																DOTA_UNIT_TARGET_TEAM_FRIENDLY, 
 																DOTA_UNIT_TARGET_HERO, 
@@ -47,42 +49,29 @@ function modifier_item_lia_pure_light:OnIntervalThink()
 																FIND_ANY_ORDER, 
 																false)
 
-		table.sort(pureLightPossibleTargets,function(a,b) return a:GetHealth() < b:GetHealth() end) 
+		table.sort(pureLightPossibleTargets,function(a,b) 
+			if a:GetHealth() == b:GetHealth() then
+				return a:GetEntityIndex() < b:GetEntityIndex() --super stabil'nost'
+			end
 
-		self:GetAbility():GetOwner().pureLightTargetCount = 0
+			return a:GetHealth() < b:GetHealth() end) 
+
+		local nTargets = 0
 
 		for _,v in pairs(pureLightPossibleTargets) do
-			if self:GetAbility():GetOwner().pureLightTargetCount < totem_targets then
+			if nTargets < totem_targets then
 				local modifier = v:FindModifierByName("modifier_item_lia_pure_light_protection")
 				if modifier then
-				  if modifier:GetCaster() == caster then
-				    v:AddNewModifier(caster,self:GetAbility(),"modifier_item_lia_pure_light_protection",nil):SetDuration(0.15, false)
-				  end
+					if modifier:GetCaster() == caster then
+						v:AddNewModifier(caster,ability,"modifier_item_lia_pure_light_protection",nil):SetDuration(0.15, false)
+						nTargets = nTargets + 1
+					end
 				else
-				  v:AddNewModifier(caster,self:GetAbility(),"modifier_item_lia_pure_light_protection",nil):SetDuration(0.15, false)
-				end
-				self:GetAbility():GetOwner().pureLightTargetCount = self:GetAbility():GetOwner().pureLightTargetCount + 1
+					v:AddNewModifier(caster,ability,"modifier_item_lia_pure_light_protection",nil):SetDuration(0.15, false)
+					nTargets = nTargets + 1
+				end	
 			end
 		end 
-
---[[		for _,v in pairs(pureLightPossibleTargets) do
-			if not v:HasModifier("modifier_item_lia_pure_light_protection") and self:GetAbility():GetOwner().pureLightTargetCount < totem_targets  then
-				v:AddNewModifier(caster,self:GetAbility(),"modifier_item_lia_pure_light_protection",{duration = 0.15})
-				self:GetAbility():GetOwner().pureLightTargetCount = self:GetAbility():GetOwner().pureLightTargetCount + 1
-			end
-
-		end 
-
-
---[[	for _,v in pairs(pureLightPossibleTargets) do
-			if v:GetHealthPercent() <= health_percent_threshold and not v:HasModifier("modifier_item_lia_pure_light_protection") and self:GetAbility():GetOwner().pureLightTargetCount < totem_targets  then
-				print(v:GetUnitName())
-				v:AddNewModifier(caster,self:GetAbility(),"modifier_item_lia_pure_light_protection",{duration = buff_duration})
-				self:GetAbility():GetOwner().pureLightTargetCount = self:GetAbility():GetOwner().pureLightTargetCount + 1
-			end
-
-		end ]]--	
-
 	end
 
 end
@@ -91,6 +80,8 @@ end
 
 
 --------------------------------------------------------------------------------------------------------------------
+
+modifier_item_lia_pure_light_protection = class({})
 
 function modifier_item_lia_pure_light_protection:DeclareFunctions()
 	local funcs = {
@@ -109,35 +100,22 @@ function modifier_item_lia_pure_light_protection:IsHidden()
 end
 
 function modifier_item_lia_pure_light_protection:OnCreated(kv)
-	self:StartIntervalThink(0.1)
 	if IsServer() then
 		self:GetParent().pureLightParticleTeher = ParticleManager:CreateParticle("particles/puck_dreamcoil_tether_pure_light.vpcf",PATTACH_POINT_FOLLOW,self:GetParent())
 		ParticleManager:SetParticleControlEnt(self:GetParent().pureLightParticleTeher, 0, self:GetAbility():GetOwner(), PATTACH_POINT_FOLLOW, "follow_origin", self:GetAbility():GetOwner():GetAbsOrigin(), true) 
 		ParticleManager:SetParticleControlEnt(self:GetParent().pureLightParticleTeher, 1, self:GetParent(), PATTACH_POINT_FOLLOW, "attach_hitloc", self:GetParent():GetAbsOrigin(), true)
 	end
+	self.damagePerc = self:GetAbility():GetSpecialValueFor("totem_damage_reduction")
 end
 
-function modifier_item_lia_pure_light_protection:OnIntervalThink()
-	if IsServer() then
-	--	if not self:GetParent():HasModifier("modifier_item_lia_pure_light_radius_check") then
-		if not self:GetParent():FindModifierByNameAndCaster("modifier_item_lia_pure_light_radius_check", self:GetParent()) then
-			self:GetParent():RemoveModifierByName("modifier_item_lia_pure_light_protection")
-		end
-		
 
-	end
-
-end
 
 function modifier_item_lia_pure_light_protection:OnDestroy()
 	if IsServer() then
 		ParticleManager:DestroyParticle(self:GetParent().pureLightParticleTeher,false)
-		self:GetAbility():GetOwner().pureLightTargetCount = self:GetAbility():GetOwner().pureLightTargetCount - 1
 	end
 end
 
 function modifier_item_lia_pure_light_protection:GetModifierIncomingDamage_Percentage()
-	local damagePerc = self:GetAbility():GetSpecialValueFor("totem_damage_reduction")
-
-	return damagePerc
+	return self.damagePerc
 end
