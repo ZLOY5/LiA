af8115d55df0c55689ce38866c4a2147ba666612
ZLOY5 <ZLOY5@ukr.net>
2016-09-11 16:01:11 +0300

    Убрал AuraFix
    Теперь предметы с аурами не будут работать из сташа

--- game/dota_addons/life_in_arena/scripts/vscripts/items/AuraFix.lua
+++
@@ -1,7 +0,0 @@
-function OnUnequip(event)
-	local itemName = event.ability:GetAbilityName()
-	local item = GetItemInInventory(event.caster,itemName)
-	if item then
-		item:ApplyDataDrivenModifier(event.caster, event.caster, event.ModifierName, nil)
-	end
-end
