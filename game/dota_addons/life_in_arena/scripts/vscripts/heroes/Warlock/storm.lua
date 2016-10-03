bef58ae9b1cb6f197eca1b1035a83bd892ada575
ZLOY5 <ZLOY5@ukr.net>
2016-10-03 20:05:45 +0300

    Шторм Колдуна

--- game/dota_addons/life_in_arena/scripts/vscripts/heroes/Warlock/storm.lua
+++ game/dota_addons/life_in_arena/scripts/vscripts/heroes/Warlock/storm.lua
@@ -1,81 +1,53 @@
 ﻿
-function stormCauseDamage(keys)
 
-	--local heroCaster = keys.heroCaster
+function Storm(keys)
 	local caster = keys.caster
 	local ability = keys.ability
-	local target = keys.target
-	local damage = keys.damage
+	local radius = ability:GetSpecialValueFor("radius")
+	local damage = ability:GetSpecialValueFor("damage")
+	local castPoint = ability:GetCursorPosition()
+	
+	local targets = FindUnitsInRadius(caster:GetTeamNumber(),
+										castPoint,
+										nil,
+										radius,
+										DOTA_UNIT_TARGET_TEAM_ENEMY, 
+										DOTA_UNIT_TARGET_BASIC + DOTA_UNIT_TARGET_HERO, 
+										DOTA_UNIT_TARGET_FLAG_NONE, 
+										FIND_ANY_ORDER, 
+										false)
 
-	--DebugDrawText(caster:GetAbsOrigin(), "1", true, 4)	
-	--caster:ReduceMana(1000)
-
-	if caster:HasModifier('modifier_warlock_storm_hero') then
-		ApplyDamage(
-		{
-			victim = target,
-			attacker = caster,
-			damage = damage,
-			damage_type = DAMAGE_TYPE_MAGICAL,
-			ability = ability
-		})
+	for _,target in pairs(targets) do
+		ApplyDamage({victim = target, attacker = caster, ability = ability, damage_type = DAMAGE_TYPE_MAGICAL, damage = damage})
 	end
 
-end
+	local particleName = "particles/units/heroes/hero_zuus/zuus_lightning_bolt.vpcf"
 
-function stormEffect(keys)
-	local caster = keys.caster
-	local ability = keys.ability
-	local radius = ability:GetSpecialValueFor( "radius" )
-	local castPoint = keys.ability.castPoint
-	local particleName = "particles/units/heroes/hero_zuus/zuus_lightning_bolt_child_b.vpcf"
-	--local particleName2 = "particles/units/heroes/hero_zuus/zuus_thundergods_wrath_start_bolt_parent.vpcf"
-	
-	--"particles/units/heroes/hero_zuus/zuus_lightning_bolt_child_b.vpcf"
-	--particles/units/heroes/hero_zuus/zuus_thundergods_wrath_start_e.vpcf
-	--
-	--particles/units/heroes/hero_zuus/zuus_thundergods_wrath_start_bolt_parent.vpcf
-	--particles/units/heroes/hero_zuus/zuus_thundergods_wrath_start_bolt_parent.vpcf
-	
 	local attackPoint1 = Vector( castPoint.x, castPoint.y, castPoint.z )
 	local attackPoint2 = Vector( castPoint.x-(radius-50), castPoint.y-(radius-50), castPoint.z )
 	local attackPoint3 = Vector( castPoint.x+(radius-50), castPoint.y-(radius-50), castPoint.z )
 	local attackPoint4 = Vector( castPoint.x+(radius-50), castPoint.y+(radius-50), castPoint.z )
 	local attackPoint5 = Vector( castPoint.x-(radius-50), castPoint.y+(radius-50), castPoint.z )
-	--
-	-- Fire effect
-	-- PATTACH_POINT
-	-- PATTACH_CUSTOMORIGIN
+
 	local fxIndex1 = ParticleManager:CreateParticle( particleName, PATTACH_POINT, caster)
 	ParticleManager:SetParticleControl( fxIndex1, 0, attackPoint1 )
+	ParticleManager:SetParticleControl( fxIndex1, 1, Vector( castPoint.x, castPoint.y, castPoint.z+1000 ) )
+	ParticleManager:ReleaseParticleIndex(fxIndex1)
 	--
-	local fxIndex2 = ParticleManager:CreateParticle( particleName, PATTACH_POINT, caster)
+	--[[local fxIndex2 = ParticleManager:CreateParticle( particleName, PATTACH_POINT, caster)
 	ParticleManager:SetParticleControl( fxIndex2, 0, attackPoint2 )
+	ParticleManager:ReleaseParticleIndex(fxIndex2)
 	--
 	local fxIndex3 = ParticleManager:CreateParticle( particleName, PATTACH_POINT, caster)
 	ParticleManager:SetParticleControl( fxIndex3, 0, attackPoint3 )
+	ParticleManager:ReleaseParticleIndex(fxIndex3)
 	--
 	local fxIndex4 = ParticleManager:CreateParticle( particleName, PATTACH_POINT, caster)
 	ParticleManager:SetParticleControl( fxIndex4, 0, attackPoint4 )
+	ParticleManager:ReleaseParticleIndex(fxIndex4)
 	--
 	local fxIndex5 = ParticleManager:CreateParticle( particleName, PATTACH_POINT, caster)
 	ParticleManager:SetParticleControl( fxIndex5, 0, attackPoint5 )
-	--
-	--
-	--
-	--local fxIndex11 = ParticleManager:CreateParticle( particleName2, PATTACH_POINT, caster)
-	--ParticleManager:SetParticleControl( fxIndex11, 1, attackPoint1 )
-	--
-	--local fxIndex12 = ParticleManager:CreateParticle( particleName2, PATTACH_POINT, caster)
-	--ParticleManager:SetParticleControl( fxIndex12, 1, attackPoint2 )
-	--
-	--local fxIndex13 = ParticleManager:CreateParticle( particleName2, PATTACH_POINT, caster)
-	--ParticleManager:SetParticleControl( fxIndex13, 1, attackPoint3 )
-	--
-	--local fxIndex14 = ParticleManager:CreateParticle( particleName2, PATTACH_POINT, caster)
-	--ParticleManager:SetParticleControl( fxIndex14, 1, attackPoint4 )
-	--
-	--local fxIndex15 = ParticleManager:CreateParticle( particleName2, PATTACH_POINT, caster)
-	--ParticleManager:SetParticleControl( fxIndex15, 1, attackPoint5 )
+	ParticleManager:ReleaseParticleIndex(fxIndex5)]]
 
 end
