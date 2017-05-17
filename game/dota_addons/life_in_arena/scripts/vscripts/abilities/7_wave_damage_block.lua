3c1a9ed115a2d2480c60710868515720df7948b8
MooMooKhan <korshunoffegor@yandex.ru>
2017-05-17 20:00:12 +0300

    1

---
+++ game/dota_addons/life_in_arena/scripts/vscripts/abilities/7_wave_damage_block.lua
@@ -0,0 +1,49 @@
+7_wave_damage_block = class({}) 
+LinkLuaModifier("modifier_7_wave_damage_block", "abilities/7_wave_damage_block.lua", LUA_MODIFIER_MOTION_NONE)
+
+function android_armor:GetIntrinsicModifierName()
+	return "modifier_7_wave_damage_block" 
+end
+
+-----------------------------------------------------------------------------------------
+
+modifier_android_armor = class({}) 
+
+function modifier_android_armor:IsHidden()
+	return true 
+end
+
+function modifier_android_armor:DeclareFunctions()
+	local funcs = {
+		MODIFIER_PROPERTY_TOTAL_CONSTANT_BLOCK_UNAVOIDABLE_PRE_ARMOR,
+	}
+ 
+	return funcs
+end
+
+function modifier_android_armor:OnCreated(kv)
+	self.damage_block = self:GetAbility():GetSpecialValueFor("damage_block")
+end
+
+function modifier_android_armor:OnRefresh(kv)
+	self.damage_block = self:GetAbility():GetSpecialValueFor("damage_block")
+	self.minimumDamage = self:GetAbility():GetSpecialValueFor("min_damage")
+end
+
+function modifier_android_armor:GetModifierPhysical_ConstantBlockUnavoidablePreArmor(params) 
+	--PrintTable("GetModifierPhysical_ConstantBlockUnavoidablePreArmor",params)
+	if params.inflictor then --не действует на физ урон от способностей
+		return 0
+	else 
+		local newDamage = params.original_damage-self.damage_block
+		if newDamage < 0 then 
+			newDamage = 0 
+		end
+		
+		if newDamage < self.minimumDamage then 
+			return self.damage_block-(self.minimumDamage-newDamage)
+		else 
+			return self.damage_block 
+		end
+	end
+end
