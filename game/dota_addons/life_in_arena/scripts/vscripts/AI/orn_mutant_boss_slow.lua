242eb9ec3825f28cff56516d7b15194a5d8cd8ad
MazalTovGeksli <kaa1994g@mail.ru>
2017-07-04 15:39:43 +0700

    orn_mutant_boss_slow_lua

---
+++ game/dota_addons/life_in_arena/scripts/vscripts/AI/orn_mutant_boss_slow.lua
@@ -0,0 +1,47 @@
+if IsClient() then return end
+require('survival/AIcreeps')
+
+function Spawn(entityKeyValues)
+	--print("Spawn")
+    thisEntity:SetHullRadius(30) 
+	if thisEntity:GetPlayerOwnerID() ~= -1 then
+		return
+	end
+	
+	ABILITY_orn_mutant_boss_slow = thisEntity:FindAbilityByName("orn_mutant_boss_slow")
+	thisEntity:SetContextThink( "8_wave_think", Think8Wave , 0.1)
+end
+
+function Think8Wave()
+	if not thisEntity:IsAlive() or thisEntity:IsIllusion() then
+		return nil 
+	end
+
+	if GameRules:IsGamePaused() then
+		return 1
+	end
+
+	if thisEntity:IsStunned() then 
+		return 2 
+	end
+
+	AICreepsAttackOneUnit({unit = thisEntity})
+	--print(Survival.AICreepCasts)
+		
+	if ABILITY_orn_mutant_boss_slow:IsFullyCastable() and Survival.AICreepCasts < Survival.AIMaxCreepCasts then
+		local targets = FindUnitsInRadius(thisEntity:GetTeam(), 
+						  thisEntity:GetOrigin(), 
+						  nil, 
+						  1000, 
+						  DOTA_UNIT_TARGET_TEAM_ENEMY, 
+						  DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC, 
+						  DOTA_UNIT_TARGET_FLAG_NONE, 
+						  FIND_ANY_ORDER, 
+						  false)
+		if #targets ~= 0 then
+			thisEntity:CastAbilityOnTarget(targets[RandomInt(1,#targets)], ABILITY_orn_mutant_boss_slow, -1)
+			Survival.AICreepCasts = Survival.AICreepCasts + 1
+		end
+	end
+	return 2
+end
