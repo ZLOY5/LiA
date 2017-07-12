9076066a1ef72d3443690ac9fc62d541ddc6dce7
ZLOY5 <ZLOY5@ukr.net>
2017-07-12 18:17:05 +0300

    Доспехи Бога
    не выпадают после перекладывания из бекпека

--- game/dota_addons/life_in_arena/scripts/vscripts/items/onlyone.lua
+++ game/dota_addons/life_in_arena/scripts/vscripts/items/onlyone.lua
@@ -10,7 +10,7 @@ function KnightCuirassShield(event)
 end
 
 function DivineArmor(event)
-	if event.caster:HasItemInInventory("item_lia_divine_armor")  then
+	if event.caster:HasItemInInventory("item_lia_divine_armor",false,event.ability) then
 		Timers:CreateTimer(0.01,function()
 			event.caster:DropItemAtPositionImmediate(event.ability, event.caster:GetAbsOrigin())
 			FireGameEvent( 'custom_error_show', { player_ID = event.caster:GetPlayerOwnerID(), _error = "#lia_hud_error_cant_have_two_divine_armor" } )
