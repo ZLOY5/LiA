3922eed9a8ab6a682a43632951af6a47dbe5d45d
Egor <MooMooKhan@users.noreply.github.com>
2017-12-15 10:37:02 +0300

    Update Eating.lua

--- game/dota_addons/life_in_arena/scripts/vscripts/heroes/NagaSiren/Eating.lua
+++ game/dota_addons/life_in_arena/scripts/vscripts/heroes/NagaSiren/Eating.lua
@@ -1,7 +1,7 @@
 function Heal( event )
 	local ability = event.ability
 	
-	if ability:IsCooldownReady() then
+	if ability:IsCooldownReady() and not event.caster:PassivesDisabled() then
   		event.caster:Heal(event.unit:GetMaxHealth()*ability:GetLevelSpecialValueFor("heal_percentage", ability:GetLevel() - 1 )*0.01, event.unit)	
   		ability:StartCooldown(ability:GetCooldown(ability:GetLevel()))
   	end
