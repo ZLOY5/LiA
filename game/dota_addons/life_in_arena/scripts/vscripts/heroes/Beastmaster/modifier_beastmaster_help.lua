7c753b551b982f4d540fd01a7016e68c8f460a0f
Egor <MooMooKhan@users.noreply.github.com>
2017-12-18 11:13:52 +0300

    Update modifier_beastmaster_help.lua

--- game/dota_addons/life_in_arena/scripts/vscripts/heroes/Beastmaster/modifier_beastmaster_help.lua
+++ game/dota_addons/life_in_arena/scripts/vscripts/heroes/Beastmaster/modifier_beastmaster_help.lua
@@ -14,6 +14,9 @@ end
 
 function modifier_beastmaster_help:OnDeath(params)
 	if IsServer() then 
+		if self:GetParent():PassivesDisabled() then
+			return
+		end
 		if params.unit:GetPlayerOwnerID() == self:GetParent():GetPlayerOwnerID() and not params.unit:IsIllusion() and not params.unit:IsHero() then
 			local heal_percent = self:GetAbility():GetSpecialValueFor("heal_percent")*0.01
 			self:GetParent():Heal(params.unit:GetMaxHealth()*heal_percent, self:GetParent())
