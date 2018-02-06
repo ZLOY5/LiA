d4a2e7469798ca0544ed7e52d5513b6d5f01810b
MooMooKhan <MooMooKhan@users.noreply.github.com>
2018-02-06 14:34:24 +0300

    Огненный Дождь Колдуна на Луа

--- game/dota_addons/life_in_arena/scripts/vscripts/heroes/Warlock/warlock_firestorm.lua
+++ game/dota_addons/life_in_arena/scripts/vscripts/heroes/Warlock/warlock_firestorm.lua
@@ -1,4 +1,6 @@
 warlock_firestorm = class({})
+LinkLuaModifier("modifier_warlock_firestorm_burn","heroes/DarkKnight/modifier_warlock_firestorm_burn.lua",LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier("modifier_warlock_firestorm_channel","heroes/DarkKnight/modifier_warlock_firestorm_channel.lua",LUA_MODIFIER_MOTION_NONE)
 
 function warlock_firestorm:GetManaCost(iLevel)
 	if self:GetCaster():HasScepter() then
@@ -8,46 +10,31 @@ function warlock_firestorm:GetManaCost(iLevel)
 	return self.BaseClass.GetManaCost( self, iLevel ) 
 end
 
-function warlock_firestorm:GetAOERadius(iLevel)
+function warlock_firestorm:GetAOERadius()
 	if self:GetCaster():HasScepter() then
 		return self:GetSpecialValueFor( "radius_scepter" )
 	end
 
-	return self.BaseClass.GetAOERadius( self, iLevel )
+	return self:GetSpecialValueFor( "radius" )
+end
+
+function warlock_firestorm:GetChannelTime()
+	self.channel_time = self:GetSpecialValueFor( "channel_time" )
+
+	return self.channel_time
+end
+
+function warlock_firestorm:OnAbilityPhaseStart()
+	self.target_point = self:GetCursorPosition()
+	EmitSoundOn( "Hero_AbyssalUnderlord.Firestorm.Start", self:GetCaster() )	
 end
 
 function warlock_firestorm:OnSpellStart()
-	
-	self.target = self:GetCursorPosition()
+	self.channel_time = self:GetSpecialValueFor( "channel_time" )
 
-	if self:GetCaster():HasScepter() then
-		self.damage = self:GetSpecialValueFor( "damage_scepter" )
-		self.radius = self:GetSpecialValueFor( "radius_scepter" )
-	else
-		self.damage = self:GetSpecialValueFor( "damage" )
-		self.radius = self:GetSpecialValueFor( "radius" )
-	end
+	self.channel_modifier = self:GetCaster():AddNewModifier(self:GetCaster(), self, "modifier_warlock_firestorm_channel", {duration = self.channel_time})
+end
 
-	local targets = FindUnitsInRadius(self:GetCaster():GetTeamNumber(),
-										self.target,
-										nil,
-										self.radius,
-										DOTA_UNIT_TARGET_TEAM_ENEMY, 
-										DOTA_UNIT_TARGET_BASIC + DOTA_UNIT_TARGET_HERO, 
-										DOTA_UNIT_TARGET_FLAG_NONE, 
-										FIND_ANY_ORDER, 
-										false)
-
-	for _,v in pairs(targets) do
-		ApplyDamage({victim = v, attacker = self:GetCaster(), ability = self, damage_type = DAMAGE_TYPE_MAGICAL, damage = self.damage})
-	end
-
-	local particleName = "particles/units/heroes/hero_zuus/zuus_lightning_bolt.vpcf"
-
-	self.FXIndex = ParticleManager:CreateParticle( particleName, PATTACH_POINT, self:GetCaster())
-	ParticleManager:SetParticleControl( self.FXIndex, 0, self.target )
-	ParticleManager:SetParticleControl( self.FXIndex, 1, Vector( self.target.x, self.target.y, self.target.z+1000 ) )
-	ParticleManager:ReleaseParticleIndex(self.FXIndex)
-
-	EmitSoundOn( "Hero_Zuus.LightningBolt", self:GetCaster() )
+function warlock_firestorm:OnChannelFinish(bInterrupted)
+	self.channel_modifier:Destroy()
 end
