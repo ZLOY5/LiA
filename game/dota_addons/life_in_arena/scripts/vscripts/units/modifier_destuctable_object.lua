96c13a2a60315bd9a4a8d5a78ddf5ef072caa8c9
Egor Korshunov <korshunoffegor@yandex.ru>
2022-11-04 19:48:08 +0100

    Shaman and an attempt to add obstructions to barrels

--- game/dota_addons/life_in_arena/scripts/vscripts/units/modifier_destuctable_object.lua
+++ game/dota_addons/life_in_arena/scripts/vscripts/units/modifier_destuctable_object.lua
@@ -9,9 +9,37 @@ function modifier_destuctable_object:IsPurgable()
 end
 
 function modifier_destuctable_object:OnCreated()
-    CreateTempTree(self:GetParent():GetAbsOrigin(), 7200)
+    if IsServer() then
+        self.parent = self:GetParent()
+        self.parent.obstruction = SpawnEntityFromTableSynchronous('point_simple_obstruction', {origin = self:GetParent():GetAbsOrigin()})
+        self.origin = self.parent:GetAbsOrigin()
+        
+        DebugDrawCircle(self.origin, Vector(120,0,0),20,100,true,100)
+    end
 end
 
-function modifier_destuctable_object:OnRemoved()
-    GridNav:DestroyTreesAroundPoint(self:GetParent():GetAbsOrigin(), 5, true)
+function modifier_destuctable_object:OnDestroy()
+    if IsServer() then
+        -- if self.parent.obstruction then
+        --     UTIL_Remove(self.parent.obstruction)
+        -- end
+        self.barrels = FindUnitsInRadius(DOTA_TEAM_GOODGUYS, 
+                            self.origin, 
+                            nil, 
+                            120, 
+                            DOTA_UNIT_TARGET_TEAM_BOTH, 
+                            DOTA_UNIT_TARGET_ALL, 
+                            DOTA_UNIT_TARGET_FLAG_NONE, 
+                            FIND_ANY_ORDER, 
+                            false)
+        print(#self.barrels)
+        if (#self.barrels) then
+            for k, v in pairs(self.barrels) do
+                print(v.obstruction)
+                if v.obstruction then
+                    UTIL_Remove(self.parent.obstruction)
+                end
+            end
+        end
+    end
 end
