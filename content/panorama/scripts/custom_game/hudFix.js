f1a8eda91f16a33b55d6b83996554afbaacbfb54
Egor Korshunov <korshunoffegor@yandex.ru>
2025-05-26 20:48:36 +0200

    Shop button fixed

--- content/panorama/scripts/custom_game/hudFix.js
+++ content/panorama/scripts/custom_game/hudFix.js
@@ -12,7 +12,7 @@ var dotaHud = $.GetContextPanel().GetParent().GetParent().GetParent().GetParent(
 
 	dotaHud.FindChildTraverse("GameInfoButton").style.visibility = "collapse";
 
-	dotaHud.FindChildTraverse("courier").style.visibility = "collapse";
+	dotaHud.FindChildTraverse("CourierControls").style.visibility = "collapse";
 	dotaHud.FindChildTraverse("CommonItems").style.visibility = "collapse";
 	dotaHud.FindChildTraverse("QuickBuySlot8").style.visibility = "collapse";
 	dotaHud.FindChildTraverse("GlyphScanContainer").style.visibility = "collapse";
