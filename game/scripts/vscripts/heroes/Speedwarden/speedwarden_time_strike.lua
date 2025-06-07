50b302920d0279f14dccaf75118af7393463778b
Egor Korshunov <korshunoffegor@yandex.ru>
2025-06-07 17:25:06 +0200

    Speedwarden Time Strike chance fix

--- game/scripts/vscripts/heroes/Speedwarden/speedwarden_time_strike.lua
+++ game/scripts/vscripts/heroes/Speedwarden/speedwarden_time_strike.lua
@@ -27,7 +27,7 @@ function speedwarden_time_strike:DealTimeStrikeDamage(caster, target, ultimate)
     if not ultimate then
         local mad_charge = caster:HasModifier("modifier_speedwarden_mad_charge_buff")
         if mad_charge then
-            chance = chance + mad_charge:GetStackCount()
+            chance = chance + chance_per_charge * mad_charge:GetStackCount()
         end
         if RollPercentage(chance) then 
             if not target:IsMagicImmune() then
