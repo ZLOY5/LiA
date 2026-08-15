db5e96581ce69488e42a9a793f93afbc8223af71
Egor Korshunov <korshunoffegor@yandex.ru>
2026-08-15 23:11:24 +0200

    Seal of Power Fix

--- game/scripts/vscripts/items/SealOfPower.lua
+++ game/scripts/vscripts/items/SealOfPower.lua
@@ -1,218 +1,118 @@
+-- Seal of Power — переключаемый предмет-конвертер статов.
+-- OFF (по умолчанию) = режим Урона: броня -> урон, штраф к броне.
+-- ON               = режим Брони: урон  -> броня, штраф к урону.
+-- Числа и формулы сохранены от старой реализации; убраны 3 предмета + подмена через SwapItems.
+
 item_lia_seal_of_power_shop = class({})
+LinkLuaModifier("modifier_item_lia_seal_of_power_shop", "items/SealOfPower.lua", LUA_MODIFIER_MOTION_NONE)
 
-function item_lia_seal_of_power_shop:OnSpellStart()
-	local caster = self:GetCaster()
-
-	caster:RemoveItem(self) 
-	Timers:CreateTimer(0.01,function()
-		local item = caster:AddItemByName("item_lia_seal_of_power_damage")
-		item:SetPurchaseTime(GameRules:GetGameTime())
-	end)
+function item_lia_seal_of_power_shop:GetIntrinsicModifierName()
+	return "modifier_item_lia_seal_of_power_shop"
 end
 
-function OnEquip(event)
-	local caster = event.caster
-
-	caster:RemoveItem(event.ability) 
-
-	Timers:CreateTimer(0.01, function()
-		local item = caster:AddItemByName("item_lia_seal_of_power_damage")
-		item:SetPurchaseTime(GameRules:GetGameTime())
-	end)
-
-end
-
----------------------------------------------------------------------------------------
---///////////////////////////////////////////////////////////////////////////////////--
----------------------------------------------------------------------------------------
-
-item_lia_seal_of_power_armor = class({})
-LinkLuaModifier("modifier_item_lia_seal_of_power_armor", "items/SealOfPower.lua", LUA_MODIFIER_MOTION_NONE)
-
-function item_lia_seal_of_power_armor:GetIntrinsicModifierName() 
-	return "modifier_item_lia_seal_of_power_armor"
-end
-
-function item_lia_seal_of_power_armor:OnSpellStart()
-	local caster = self:GetCaster()
-	local purchaseTime = self:GetPurchaseTime()
-	local itemSlot1 = self:GetItemSlot()
-
-	caster:RemoveItem(self)
-
-	local item = caster:AddItemByName("item_lia_seal_of_power_damage")
-	item:SetPurchaseTime(purchaseTime)
-
-	local itemSlot2 = item:GetItemSlot()
-	caster:SwapItems(itemSlot1,itemSlot2)
+-- Мгновенный пересчёт при переключении режима (без ожидания тика).
+function item_lia_seal_of_power_shop:OnToggle()
+	local mod = self:GetCaster():FindModifierByName("modifier_item_lia_seal_of_power_shop")
+	if mod then mod:OnIntervalThink() end
 end
 
 ---------------------------------------------------------------------------------------
 
-modifier_item_lia_seal_of_power_armor = class({})
+modifier_item_lia_seal_of_power_shop = class({})
 
-function modifier_item_lia_seal_of_power_armor:GetAttributes() 
-	return MODIFIER_ATTRIBUTE_PERMANENT 
-end
+function modifier_item_lia_seal_of_power_shop:GetAttributes() return MODIFIER_ATTRIBUTE_PERMANENT end
+function modifier_item_lia_seal_of_power_shop:IsHidden()      return false end
+function modifier_item_lia_seal_of_power_shop:IsPurgable()    return false end
 
-function modifier_item_lia_seal_of_power_armor:IsHidden()
-	return false
-end
+function modifier_item_lia_seal_of_power_shop:OnCreated()
+	local a = self:GetAbility()
 
-function modifier_item_lia_seal_of_power_armor:IsPurgable()
-	return false
-end
+	self.bonusDamage          = a:GetSpecialValueFor("bonus_damage")
+	self.bonusArmor           = a:GetSpecialValueFor("bonus_armor")
+	self.armorNeededForDamage = a:GetSpecialValueFor("armor_needed_for_one_damage") -- 0.5 (режим Урона)
+	self.damageNeededForArmor = a:GetSpecialValueFor("damage_needed_for_one_armor") -- 20  (режим Брони)
+	self.damageLimit          = a:GetSpecialValueFor("damage_limit")
+	self.armorLimit           = a:GetSpecialValueFor("armor_limit")
+	self.loseFactor           = a:GetSpecialValueFor("stats_lose_percent") * 0.01
 
-function modifier_item_lia_seal_of_power_armor:DeclareFunctions()
-	local funcs = {
-		MODIFIER_PROPERTY_PREATTACK_BONUS_DAMAGE,
-		MODIFIER_PROPERTY_PHYSICAL_ARMOR_BONUS,
-	}
- 
-	return funcs
-end
+	-- Активна всегда только одна пара (по режиму), вторая обнулена.
+	self.damageFromArmor = 0 -- прибавка к урону в режиме Урона
+	self.armorLose       = 0 -- штраф к броне  в режиме Урона (<=0)
+	self.armorFromDamage = 0 -- прибавка к броне в режиме Брони
+	self.damageLose      = 0 -- штраф к урону   в режиме Брони (<=0)
 
-function modifier_item_lia_seal_of_power_armor:OnCreated(kv)
-	self.damageBonus = self:GetAbility():GetSpecialValueFor("bonus_damage")
-	self.armorBonus = self:GetAbility():GetSpecialValueFor("bonus_armor")
-	self.oneArmorDamage = self:GetAbility():GetSpecialValueFor("damage_needed_for_one_armor")
-	self.armorLimit = self:GetAbility():GetSpecialValueFor("armor_limit")
-	self.damageLoseFactor = self:GetAbility():GetSpecialValueFor("stats_lose_percent")*0.01
-	
-	self.armorBonusFromDamage = 0
-	self.damageLose = 0
+	self.abilityID = a:entindex()
 
-	self.abilityID = self:GetAbility():entindex()
-	
 	self:StartIntervalThink(0.1)
 	self:OnIntervalThink()
 end
 
-function modifier_item_lia_seal_of_power_armor:GetModifierPreAttack_BonusDamage(params)
-	return self.damageBonus + self.damageLose
-end
+function modifier_item_lia_seal_of_power_shop:OnIntervalThink()
+	local a = self:GetAbility()
+	if not a or a:IsNull() then return end
+	local parent = self:GetParent()
 
-function modifier_item_lia_seal_of_power_armor:GetModifierPhysicalArmorBonus(params)
-	return self.armorBonus + self.armorBonusFromDamage
-end
-
-function modifier_item_lia_seal_of_power_armor:OnIntervalThink()
 	if IsServer() then
-		local damage = math.floor(self:GetCaster():GetAverageTrueAttackDamage(nil) - self.damageLose + 0.5)
-		
-		self.armorBonusFromDamage = damage/self.oneArmorDamage
-		
-		if self.armorBonusFromDamage > self.armorLimit then
-			self.armorBonusFromDamage = self.armorLimit
+		if a:GetToggleState() then
+			-- Режим Брони: урон -> броня
+			local damage = math.floor(parent:GetAverageTrueAttackDamage(nil) - self.damageLose + 0.5)
+			local armorFromDamage = damage / self.damageNeededForArmor
+			if armorFromDamage > self.armorLimit then armorFromDamage = self.armorLimit end
+
+			self.armorFromDamage = armorFromDamage
+			self.damageLose      = math.floor(-(armorFromDamage * self.damageNeededForArmor * self.loseFactor) + 0.5)
+			self.damageFromArmor = 0
+			self.armorLose       = 0
+
+			self:SetStackCount(math.floor(armorFromDamage + 0.5))
+		else
+			-- Режим Урона: броня -> урон (учитываем броню до штрафа самого предмета)
+			local armor = parent:GetPhysicalArmorValue(false) - self.armorLose
+			local damageFromArmor = armor / self.armorNeededForDamage
+			if damageFromArmor > self.damageLimit then damageFromArmor = self.damageLimit end
+
+			self.damageFromArmor = damageFromArmor
+			self.armorLose       = -(damageFromArmor * self.armorNeededForDamage * self.loseFactor)
+			self.armorFromDamage = 0
+			self.damageLose      = 0
+
+			self:SetStackCount(math.floor(damageFromArmor + 0.5))
 		end
 
-		self.damageLose = math.floor(-(self.armorBonusFromDamage*self.oneArmorDamage*self.damageLoseFactor) + 0.5)
-		
-		--print(damage,self.armorBonusFromDamage,self.damageLose)
-		
-		self:SetStackCount(math.floor(self.armorBonusFromDamage+0.5))
-		CustomNetTables:SetTableValue("custom_modifier_state",tostring(self.abilityID),{armorBonusFromDamage = self.armorBonusFromDamage, damageLose = self.damageLose})
-	end
-
-	if IsClient() then
-		local netTable = CustomNetTables:GetTableValue("custom_modifier_state",tostring(self.abilityID))
-		if netTable then
-			self.armorBonusFromDamage = netTable.armorBonusFromDamage
-			self.damageLose = netTable.damageLose
-			--print("Client",self.armorBonusFromDamage,self.damageLose)
+		CustomNetTables:SetTableValue("custom_modifier_state", tostring(self.abilityID), {
+			damageFromArmor = self.damageFromArmor,
+			armorLose       = self.armorLose,
+			armorFromDamage = self.armorFromDamage,
+			damageLose      = self.damageLose,
+		})
+	else
+		local nt = CustomNetTables:GetTableValue("custom_modifier_state", tostring(self.abilityID))
+		if nt then
+			self.damageFromArmor = nt.damageFromArmor or 0
+			self.armorLose       = nt.armorLose or 0
+			self.armorFromDamage = nt.armorFromDamage or 0
+			self.damageLose      = nt.damageLose or 0
 		end
 	end
 end
 
----------------------------------------------------------------------------------------
---///////////////////////////////////////////////////////////////////////////////////--
----------------------------------------------------------------------------------------
-
-item_lia_seal_of_power_damage = class({})
-LinkLuaModifier("modifier_item_lia_seal_of_power_damage", "items/SealOfPower.lua", LUA_MODIFIER_MOTION_NONE)
-
-function item_lia_seal_of_power_damage:GetIntrinsicModifierName() 
-	return "modifier_item_lia_seal_of_power_damage"
-end
-
-function item_lia_seal_of_power_damage:OnSpellStart()
-	local caster = self:GetCaster()
-	local purchaseTime = self:GetPurchaseTime()
-	local itemSlot1 = self:GetItemSlot()
-
-	caster:RemoveItem(self)
-
-	local item = caster:AddItemByName("item_lia_seal_of_power_armor")
-	item:SetPurchaseTime(purchaseTime)
-
-	local itemSlot2 = item:GetItemSlot()
-	caster:SwapItems(itemSlot1,itemSlot2)
-end
-
----------------------------------------------------------------------------------------
-
-modifier_item_lia_seal_of_power_damage = class({})
-
-function modifier_item_lia_seal_of_power_damage:GetAttributes() 
-	return MODIFIER_ATTRIBUTE_PERMANENT 
-end
-
-function modifier_item_lia_seal_of_power_damage:IsHidden()
-	return false
-end
-
-function modifier_item_lia_seal_of_power_damage:IsPurgable()
-	return false
-end
-
-function modifier_item_lia_seal_of_power_damage:DeclareFunctions()
-	local funcs = {
+function modifier_item_lia_seal_of_power_shop:DeclareFunctions()
+	return {
 		MODIFIER_PROPERTY_PREATTACK_BONUS_DAMAGE,
 		MODIFIER_PROPERTY_PHYSICAL_ARMOR_BONUS,
 	}
- 
-	return funcs
 end
 
-function modifier_item_lia_seal_of_power_damage:OnCreated(kv)
-	self.damageBonus = self:GetAbility():GetSpecialValueFor("bonus_damage")
-	self.armorBonus = self:GetAbility():GetSpecialValueFor("bonus_armor")
-	self.oneArmorDamage = self:GetAbility():GetSpecialValueFor("armor_needed_for_one_damage")
-	self.damageLimit = self:GetAbility():GetSpecialValueFor("damage_limit")
-	self.armorLoseFactor = self:GetAbility():GetSpecialValueFor("stats_lose_percent")*0.01
-	
-	self.damageBonusFromArmor = 0
-	self.armorLose = 0
-	
-	self:StartIntervalThink(0.1)
-	self:OnIntervalThink()
+function modifier_item_lia_seal_of_power_shop:GetModifierPreAttack_BonusDamage()
+	return self.bonusDamage + self.damageFromArmor + self.damageLose
 end
 
-function modifier_item_lia_seal_of_power_damage:OnIntervalThink()
-	local armor = self:GetCaster():GetPhysicalArmorValue(false) - self.armorLose --броню отнятую самим скиллом не учитываем
-	
-	self.damageBonusFromArmor = armor/self.oneArmorDamage
-	
-	if self.damageBonusFromArmor > self.damageLimit then
-		self.damageBonusFromArmor = self.damageLimit
-	end
+function modifier_item_lia_seal_of_power_shop:GetModifierPhysicalArmorBonus()
+	return self.bonusArmor + self.armorLose + self.armorFromDamage
+end
 
-	self.armorLose = -(self.damageBonusFromArmor*self.oneArmorDamage*self.armorLoseFactor)
-	
+function modifier_item_lia_seal_of_power_shop:OnDestroy()
 	if IsServer() then
-		self:SetStackCount(math.floor(self.damageBonusFromArmor+0.5))
+		CustomNetTables:SetTableValue("custom_modifier_state", tostring(self.abilityID), {})
 	end
 end
-
-function modifier_item_lia_seal_of_power_damage:GetModifierPreAttack_BonusDamage(params)
-	return self.damageBonus + self.damageBonusFromArmor
-end
-
-function modifier_item_lia_seal_of_power_damage:GetModifierPhysicalArmorBonus(params)
-	return self.armorBonus + self.armorLose
-end
-
-
-
----------------------------------------------------------------------------------------
-
